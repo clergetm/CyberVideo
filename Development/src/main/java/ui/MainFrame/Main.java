81c0ecb64681e40b168641580142b1aa137b3125
Mathys Clerget <mathysclerget+github@gmail.com>
2022-10-30 20:05:27 +0100

    feat: Add Main Page

---
+++ Development/src/main/java/ui/MainFrame/Main.java
@@ -0,0 +1,49 @@
+package ui.MainFrame;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * @author MathysC
+ * Main Class of AL2000. Initialize the machine Interface.
+ */
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+
+	/**
+	 * Constructor of Main.
+	 * Initialize the AL200 Main Window.
+	 * @author MathysC
+	 *
+	 */
+	public Main(){
+		super("AL2000");
+		// Set options.
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        
+        // Maximize the Window.
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+	}
+
+
+
+	/**
+	 * Run the Main Window of AL2000.
+	 * @author MathysC
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() { 
+				new Main().setVisible(true); 
+			}
+		});
+
+	}
+
+}
