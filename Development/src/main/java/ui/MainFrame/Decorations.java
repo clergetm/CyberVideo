1bb5ed59f93216e9487ba281c9caa9a1b5da24fa
Mathys Clerget <mathysclerget+github@gmail.com>
2022-10-31 18:27:02 +0100

    doc(fix): Fix indents

--- Development/src/main/java/ui/MainFrame/Decorations.java
+++ Development/src/main/java/ui/MainFrame/Decorations.java
@@ -16,63 +16,62 @@ import javax.swing.border.EmptyBorder;
  */
 enum Decorations {
 
-	// Dimensions
-	DIM_BUTTON(new Dimension(100,100)),
-	DIM_LOGO(new Dimension(200,150)),
-	
-	// Icons
-	ICON_LOGO("Test_Logo"),					// TODO
-	ICON_BUTTON("Test_Button"),				// TODO
-	ICON_BANNER("Test_Banner"),				// TODO
-	
-	// Borders
-	BORDER_DEFAULT(new EmptyBorder(10, 10, 10, 10));
-	
-	private Object decoration;
-	private static String PREFIXPATH = "Resources/";
-	private static String FORMATIMAGE = ".png";
-	
-	
-	Decorations(Object o) {
-		this.decoration = o; 
-	}
-	
-	/**
-	 * Get the enumerated Dimension
-	 * @author MathysC
-	 *
-	 * @return Dimension if the enumerated element name’s start with "DIM_";
-	 */
-	public Dimension getDimension() {
-		if(this.name().startsWith("DIM_"))
-				return (Dimension) this.decoration;
-		else
-			return null;
-	}
-	
-	/**
-	 * Get the enumerated Icon
-	 * @author MathysC
-	 *
-	 * @return ImageIcon if the enumerated element name’s start with "ICON_";
-	 */
-	public ImageIcon getIcon() {
-		if(this.name().startsWith("ICON_"))
-			return new ImageIcon(PREFIXPATH + (String)this.decoration + FORMATIMAGE);
-		else
-			return null;
-	}
-	
-	/**
-	 * Get the enumerated Border
-	 * @author MathysC
-	 *
-	 * @return Border if the enumerated element name’s start with "BORDER_";
-	 */
-	public Border getBorder() {
-		if(this.name().startsWith("BORDER_"))
-			return (Border) this.decoration;
-		else
-			return null;
-	}
+    // Dimensions
+    DIM_BUTTON(new Dimension(100, 100)),
+        DIM_LOGO(new Dimension(200, 150)),
+
+        // Icons
+        ICON_LOGO("Test_Logo"), // TODO
+        ICON_BUTTON("Test_Button"), // TODO
+        ICON_BANNER("Test_Banner"), // TODO
+
+        // Borders
+        BORDER_DEFAULT(new EmptyBorder(10, 10, 10, 10));
+
+    private Object decoration;
+    private static String PREFIXPATH = "resources/";
+    private static String FORMATIMAGE = ".png";
+
+    Decorations(Object o) {
+        this.decoration = o;
+    }
+
+    /**
+     * Get the enumerated Dimension
+     * @author MathysC
+     *
+     * @return Dimension if the enumerated element name’s start with "DIM_";
+     */
+    public Dimension getDimension() {
+        if (this.name().startsWith("DIM_"))
+            return (Dimension) this.decoration;
+        else
+            return null;
+    }
+
+    /**
+     * Get the enumerated Icon
+     * @author MathysC
+     *
+     * @return ImageIcon if the enumerated element name’s start with "ICON_";
+     */
+    public ImageIcon getIcon() {
+        if (this.name().startsWith("ICON_"))
+            return new ImageIcon(PREFIXPATH + (String) this.decoration + FORMATIMAGE);
+        else
+            return null;
+    }
+
+    /**
+     * Get the enumerated Border
+     * @author MathysC
+     *
+     * @return Border if the enumerated element name’s start with "BORDER_";
+     */
+    public Border getBorder() {
+        if (this.name().startsWith("BORDER_"))
+            return (Border) this.decoration;
+        else
+            return null;
+    }
 }
