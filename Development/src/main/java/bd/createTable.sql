3c078a3137ffbb54095c8c4539ebf217867da84d
Evan Pereira <evan17.pereira@gmail.com>
2022-11-01 20:32:04 +0100

    Creating tables completed

--- Development/src/main/java/bd/createTable.sql
+++ Development/src/main/java/bd/createTable.sql
@@ -1,3 +1,23 @@
+DROP TABLE IF EXISTS Films;
+DROP TABLE IF EXISTS QRCodes;
+DROP TABLE IF EXISTS BluRays;
+DROP TABLE IF EXISTS Actors;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS FilmsActors;
+DROP TABLE IF EXISTS FilmsCategories;
+DROP TABLE IF EXISTS SupportFilms;
+DROP TABLE IF EXISTS Rentals;
+DROP TABLE IF EXISTS SupportCards;
+DROP TABLE IF EXISTS SubscriberCards;
+DROP TABLE IF EXISTS Subscribers;
+DROP TABLE IF EXISTS CreditCards;
+DROP TABLE IF EXISTS HistoricCreditCards;
+DROP TABLE IF EXISTS Logins;
+DROP TABLE IF EXISTS Administrators;
+DROP TABLE IF EXISTS OwnedCards;
+DROP TABLE IF EXISTS RestrictedCategories;
+
+
 CREATE TABLE Films{
     filmID INTEGER(10),
     title VARCHAR(50),
@@ -13,7 +33,8 @@ CREATE TABLE QRCodes{
     filmID INTEGER(10),
     link VARCHAR(255),
     CONSTRAINT QRCodeID_unique UNIQUE (QRCodeID),
-    CONSTRAINT QRCodeID_pk PRIMARY KEY (QRCodeID)
+    CONSTRAINT QRCodeID_pk PRIMARY KEY (QRCodeID),
+    CONSTRAINT filmID_fk FOREIGN KEY (filmID) REFERENCES Films(FilmID)
 };
 
 CREATE TABLE BluRays{
@@ -21,7 +42,8 @@ CREATE TABLE BluRays{
     filmID INTEGER(10),
     price FLOAT(3),
     state VARCHAR(9) CHECK (state IN ('AVAILABLE' ,'RENTED', 'DAMAGED', 'STOLEN') ),
-    CONSTRAINT blurayID_pk PRIMARY KEY (blurayID)
+    CONSTRAINT blurayID_pk PRIMARY KEY (blurayID),
+    CONSTRAINT filmID_fk FOREIGN KEY (filmID) REFERENCES Films(FilmID)
 };
 
 CREATE TABLE Actors{
@@ -42,14 +64,18 @@ CREATE TABLE Categories{
 CREATE TABLE FilmsCategories{
     filmID INTEGER(10),
     categorieID INTEGER(10),
-    CONSTRAINT filmID_categorieID_pk PRIMARY KEY (filmID,categorieID)
+    CONSTRAINT filmID_categorieID_pk PRIMARY KEY (filmID,categorieID),
+    CONSTRAINT filmID_FilmsCategories_fk FOREIGN KEY (filmID) REFERENCES Films(FilmID),
+    CONSTRAINT categorieID_fk FOREIGN KEY (categorieID) REFERENCES Categories(categorieID)
 };
 
 
 CREATE TABLE FilmsActors{
     filmID INTEGER(10),
     actorID INTEGER(10),
-    CONSTRAINT filmID_actorID_pk PRIMARY KEY (filmID,actorID)
+    CONSTRAINT filmID_actorID_pk PRIMARY KEY (filmID,actorID),
+    CONSTRAINT filmID_FilmsActors_fk FOREIGN KEY (filmID) REFERENCES Films(FilmID),
+    CONSTRAINT actorID_fk FOREIGN KEY (actorID) REFERENCES Actors(actorID)
 };
 
 
@@ -57,7 +83,8 @@ CREATE TABLE SupportFilms{
     supportFilmID INTEGER(10),
     filmID INTEGER(10),
     typeFilm CHAR(2) CHECK (typeFilm IN ('QR', 'BR')),
-    CONSTRAINT supportFilmID_pk PRIMARY KEY (supportFilmID)
+    CONSTRAINT supportFilmID_pk PRIMARY KEY (supportFilmID),
+    CONSTRAINT filmID_support_fk FOREIGN KEY (filmID) REFERENCES Films(filmID)
 }
 
 CREATE TABLE Rentals{
@@ -66,7 +93,9 @@ CREATE TABLE Rentals{
     supportFilmID INTEGER(10),
     beginDate DATE,
     endDate DATE,
-    CONSTRAINT rentalsID_pk PRIMARY KEY (rentalsID)
+    CONSTRAINT rentalsID_pk PRIMARY KEY (rentalsID),
+    CONSTRAINT supportCardID_Rentals_fk FOREIGN KEY (supportCardID) REFERENCES SupportCards(supportCardID),
+    CONSTRAINT supportFIlmsID_Rentals_fk FOREIGN KEY (supportFilmID) REFERENCES SupportFilms(supportFilmID)
 };
 
 CREATE TABLE SupportCards{
@@ -81,13 +110,24 @@ CREATE TABLE SubscriberCards {
     limitWeek INTEGER(2),
     restrictedAge CHAR(3) CHECK (restrictedAge IN ('M10', 'M12', 'M16', 'M18', 'ALL') ),
     balance FLOAT(3),
-    CONSTRAINT subCardID_pk PRIMARY KEY (subCardID)
+    CONSTRAINT subCardID_pk PRIMARY KEY (subCardID),
+    CONSTRAINT supportCardID_SubscribersCards_fk FOREIGN KEY (supportCardID) REFERENCES SupportCards(supportCardID)
+};
+
+CREATE TABLE CreditCards {
+    creditID INTEGER(10),
+    supportCardID INTEGER(10),
+    creditCardNum INTEGER(16),
+    CONSTRAINT creditCardNum_unique UNIQUE (creditCardNum),
+    CONSTRAINT creditID_pk PRIMARY KEY (creditID)
 };
 
 CREATE TABLE RestrictedCategories{
     categorieID INTEGER(10),
     subCardID INTEGER(10),
-    CONSTRAINT categorieID_subCardID_pk PRIMARY KEY (categorieID,subCardID)
+    CONSTRAINT categorieID_subCardID_pk PRIMARY KEY (categorieID,subCardID),
+    CONSTRAINT categorieID_fk FOREIGN KEY (categorieID) REFERENCES Categories(categorieID),
+    CONSTRAINT subCardID_fk FOREIGN KEY (subCardID) REFERENCES SubscriberCards(subCardID)
 };
 
 CREATE TABLE Subscribers {
@@ -97,15 +137,9 @@ CREATE TABLE Subscribers {
     subFirstName VARCHAR(30),
     subLastName VARCHAR(30),
     birthDate DATE,
-    CONSTRAINT subID_pk PRIMARY KEY (subID)
-};
-
-CREATE TABLE CreditCards {
-    creditID INTEGER(10),
-    supportCardID INTEGER(10),
-    creditCardNum INTEGER(16),
-    CONSTRAINT creditCardNum_unique UNIQUE (creditCardNum),
-    CONSTRAINT creditID_pk PRIMARY KEY (creditID)
+    CONSTRAINT subID_pk PRIMARY KEY (subID),
+    CONSTRAINT loginID_Subscribers_fk FOREIGN KEY (loginID) REFERENCES Logins(loginID),
+    CONSTRAINT creditID_fk FOREIGN KEY (creditID) REFERENCES CreditCards(creditID)
 };
 
 CREATE TABLE HistoricCreditCards{
@@ -126,11 +160,14 @@ CREATE TABLE Logins{
 CREATE TABLE Administrators{
     adminID INTEGER(10),
     loginID VARCHAR(50),
-    CONSTRAINT adminID_pk PRIMARY KEY (adminID)
+    CONSTRAINT adminID_pk PRIMARY KEY (adminID),
+    CONSTRAINT loginID_Administrators_fk FOREIGN KEY (loginID) REFERENCES Logins(loginID) 
 }
 
 CREATE TABLE OwnedCards{
     subID INTEGER(10),
     subCardID INTEGER(10),
-    CONSTRAINT subID_subCardID_pk PRIMARY KEY (subID,subCardID)
+    CONSTRAINT subID_subCardID_pk PRIMARY KEY (subID,subCardID),
+    CONSTRAINT subID_fk FOREIGN KEY (subID) REFERENCES Subscribers(subID),
+    CONSTRAINT subCardID_fk FOREIGN KEY (subCardID) REFERENCES SubscriberCards(subCardID)
 };
