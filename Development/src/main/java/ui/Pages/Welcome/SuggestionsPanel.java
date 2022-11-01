a7399d723aaa12062a4cd89da3b11c1b0c463353
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2022-11-01 15:04:26 +0100

    feat: Suggestions Panel

--- Development/src/main/java/ui/Pages/Welcome/SuggestionsPanel.java
+++ Development/src/main/java/ui/Pages/Welcome/SuggestionsPanel.java
@@ -1,8 +1,23 @@
 package ui.Pages.Welcome;
 
+import java.util.ResourceBundle;
+
 import javax.swing.JPanel;
 
+import ui.Bundles.Multilingual;
+
+/**
+ * This class instantiate a list of suggested films.
+ * @author MathysC
+ *
+ */
 @SuppressWarnings("serial")
-class SuggestionsPanel extends JPanel {
+class SuggestionsPanel extends JPanel implements Multilingual {
+
+	@Override
+	public void setLanguage(ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
