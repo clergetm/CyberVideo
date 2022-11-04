4d9181312d7f120e6b8ce29db71951db021de092
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-04 10:41:34 +0100

    feat: implements Sounds in a thread

--- Development/src/main/java/ui/MainFrame/TopBar.java
+++ Development/src/main/java/ui/MainFrame/TopBar.java
@@ -21,51 +21,60 @@ import javax.swing.JButton;
  *
  */
 @SuppressWarnings("serial")
-public class TopBar extends JPanel{
+public class TopBar extends JPanel {
 
     protected JPanel topBar = new JPanel(), options = new JPanel();
     protected JLabel logo = new JLabel(), test = new JLabel();
-    protected JButton colorSwitch = new JButton(), languageSwitch = new JButton();
-    
+    protected JButton colorSwitch = new JButton(), languageSwitch = new JButton(), askForHelp = new JButton();
+
     /*Actions*/
     static final String SWITCHFR = "Switch_to_fr";
     static final String SWITCHEN = "Switch_to_en";
-	/**
-	 * Constructor of TopBar.
-	 * Set JPanel options and add components.
-	 * @author MathysC
-	 *
-	 */
-	public TopBar() {
-		// Set JPanel’s options.
-		this.setLayout(new BorderLayout());
-		this.setBorder(BorderFactory.createMatteBorder(0,0,2,0,Color.BLACK));
-		
-		// Logo.
-		logo.setPreferredSize(Decorations.DIM_LOGO.getDimension());
-		logo.setIcon(Decorations.IMG_LOGO.getImg());
-		logo.setBorder(Decorations.BORDER_DEFAULT.getBorder());
-		this.add(logo,BorderLayout.WEST);
+    static final String HELP = "Ask_for_help";
+    /**
+     * Constructor of TopBar.
+     * Set JPanel options and add components.
+     * @author MathysC
+     *
+     */
+    public TopBar() {
+        // Set JPanel’s options.
+        this.setLayout(new BorderLayout());
+        this.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
 
-		// Buttons’ JPanel.
-		options.setLayout(new FlowLayout(FlowLayout.TRAILING));
-		options.setBorder(Decorations.BORDER_DEFAULT.getBorder());
-		this.add(options,BorderLayout.EAST);
+        // Logo.
+        logo.setPreferredSize(Decorations.DIM_LOGO.getDimension());
+        logo.setIcon(Decorations.IMG_LOGO.getImg());
+        logo.setBorder(Decorations.BORDER_DEFAULT.getBorder());
+        this.add(logo, BorderLayout.WEST);
 
-		// Color switch button.
-		colorSwitch.setPreferredSize(Decorations.DIM_BUTTON.getDimension());
-		colorSwitch.setIcon(Decorations.IMG_CURRENTLIGHT.getImg());
-		colorSwitch.setContentAreaFilled(false); // transparent
-		colorSwitch.setBorderPainted(false); // without border
-		options.add(colorSwitch);
-		
-		// Language switch button.
-		languageSwitch.setPreferredSize(Decorations.DIM_BUTTON.getDimension());
-		languageSwitch.setIcon(Decorations.IMG_CURRENTEN.getImg());
-		languageSwitch.setActionCommand(SWITCHFR);
-		languageSwitch.setContentAreaFilled(false); // transparent
-		languageSwitch.setBorderPainted(false); // without border
-		options.add(languageSwitch);
+        // Buttons’ JPanel.
+        options.setLayout(new FlowLayout(FlowLayout.TRAILING));
+        options.setBorder(Decorations.BORDER_DEFAULT.getBorder());
+        this.add(options, BorderLayout.EAST);
 
-	}
+        // Color switch button.
+        colorSwitch.setPreferredSize(Decorations.DIM_BUTTON.getDimension());
+        colorSwitch.setIcon(Decorations.IMG_CURRENTLIGHT.getImg());
+        colorSwitch.setContentAreaFilled(false); // transparent
+        colorSwitch.setBorderPainted(false); // without border
+        options.add(colorSwitch);
+
+        // Language switch button.
+        languageSwitch.setPreferredSize(Decorations.DIM_BUTTON.getDimension());
+        languageSwitch.setIcon(Decorations.IMG_CURRENTEN.getImg());
+        languageSwitch.setActionCommand(SWITCHFR);
+        languageSwitch.setContentAreaFilled(false); // transparent
+        languageSwitch.setBorderPainted(false); // without border
+        options.add(languageSwitch);
+
+        // ask for help button.
+        askForHelp.setPreferredSize(Decorations.DIM_BUTTON.getDimension());
+        askForHelp.setIcon(Decorations.IMG_BUTTON.getImg());
+        askForHelp.setActionCommand(HELP);
+        askForHelp.setContentAreaFilled(false); // transparent
+        askForHelp.setBorderPainted(false); // without border
+        options.add(askForHelp);
+
+    }
 }
