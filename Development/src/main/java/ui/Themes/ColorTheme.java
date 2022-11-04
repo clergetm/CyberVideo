4b8f69dc590ef50d6c3a73f53cdb226050097593
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-05 00:48:55 +0100

    feat: Add ColorTheme Interface

---
+++ Development/src/main/java/ui/Themes/ColorTheme.java
@@ -0,0 +1,25 @@
+package ui.Themes;
+
+
+/**
+ * ColorTheme is an interface that implements two methods {@code setLight} and {@code setDark} 
+ * used to change the theme the UI.
+ * @author MathysC
+ *
+ */
+public interface ColorTheme {
+	
+	/**
+	 * Set the Light Theme.
+	 * @author MathysC
+	 *
+	 */
+	void setLight();
+	
+	/**
+	 * Set the Dark Theme.
+	 * @author MathysC
+	 *
+	 */
+	void setDark();
+}
