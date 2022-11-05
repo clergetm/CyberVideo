39b8f8f48c8ff5c49e354f12a47bd20d845d8983
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-05 22:42:28 +0100

    feat: Light Color Palette

---
+++ Development/src/main/java/ui/Palettes/LightPalette.java
@@ -0,0 +1,64 @@
+package ui.Palettes;
+
+import java.awt.Color;
+
+/**
+ * Light Color Palette.
+ * 
+ * @author MathysC
+ *
+ */
+public enum LightPalette {
+	BG(238, 239, 240),
+	FG_BLUE(47, 83, 170),	// Foreground Blue.
+	WHITE(255, 255, 255),	// Used as Foreground too.
+	BLUE(47, 63, 170);
+	
+	
+	
+	private int[] rgb;
+	
+	LightPalette(int r, int g, int b) {
+		this.rgb = new int[]{r, g, b};
+	}
+	
+	/**
+	 * Get the color.
+	 * @author MathysC
+	 *
+	 * @return Color from rgb.
+	 */
+	public Color getColor() {
+		return new Color(this.r(), this.g(), this.b());
+	}
+	
+	/**
+	 * Get red value
+	 * @author MathysC
+	 *
+	 * @return int red
+	 */
+	private int r() {
+		return this.rgb[0];
+	}
+	
+	/**
+	 * Get green value
+	 * @author MathysC
+	 *
+	 * @return int green
+	 */
+	private int g() {
+		return this.rgb[1];
+	}
+	
+	/**
+	 * Get blue value
+	 * @author MathysC
+	 *
+	 * @return int blue
+	 */
+	private int b() {
+		return this.rgb[2];
+	}
+}
