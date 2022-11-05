0d9cda721dbd3929d1ee143f49300d8ea3d4a2f8
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-05 02:35:58 +0100

    feat: Dark Theme SuggestionsPanel

--- Development/src/main/java/ui/Pages/Welcome/SuggestionsPanel.java
+++ Development/src/main/java/ui/Pages/Welcome/SuggestionsPanel.java
@@ -1,6 +1,7 @@
 package ui.Pages.Welcome;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -14,6 +15,7 @@ import javax.swing.JScrollPane;
 import ui.Decorations;
 import ui.FilmManager;
 import ui.Bundles.Multilingual;
+import ui.Palettes.DarkPalette;
 import ui.Themes.ColorTheme;
 
 /**
@@ -26,6 +28,7 @@ class SuggestionsPanel extends JPanel implements Multilingual, ColorTheme {
 
 	/* Components */
     protected JLabel sugLabel = new JLabel();
+    private JPanel panelForLabel = new JPanel(new FlowLayout());
     protected JScrollPane filmsPane;
     private FilmManager manager;
 
@@ -40,10 +43,9 @@ class SuggestionsPanel extends JPanel implements Multilingual, ColorTheme {
         this.setLayout(new BorderLayout());
 
         // Label
-        JPanel tempPanelForLabel = new JPanel(new FlowLayout());
-        tempPanelForLabel.add(sugLabel);
+        panelForLabel.add(sugLabel);
         sugLabel.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 20));
-        this.add(tempPanelForLabel, BorderLayout.NORTH);
+        this.add(panelForLabel, BorderLayout.NORTH);
 
         // JScrollPane
         manager = new FilmManager(new GridLayout(0, 4));
@@ -59,14 +61,36 @@ class SuggestionsPanel extends JPanel implements Multilingual, ColorTheme {
 
 	@Override
 	public void setLight() {
-		// TODO Auto-generated method stub
+		// This JPanel.
+        this.setBackground(null);
+        
+        // Suggestion Label.
+		this.sugLabel.setForeground(Color.BLACK);
+		this.panelForLabel.setBackground(null);
 		
+		// Film ScrollPane.
+		this.filmsPane.setBackground(null);
+		this.filmsPane.getVerticalScrollBar().setBackground(null);
+
+		// Films manager.
+		this.manager.setLight();
 	}
 
 	@Override
 	public void setDark() {
-		// TODO Auto-generated method stub
+		// This JPanel.
+		this.setBackground(DarkPalette.BG.getColor());
 		
+        // Suggestion Label.
+		this.sugLabel.setForeground(DarkPalette.FOREGROUNG.getColor());
+		this.panelForLabel.setBackground(DarkPalette.BG.getColor());
+		
+		// Film ScrollPane.
+		this.filmsPane.setBackground(DarkPalette.BG.getColor());
+		this.filmsPane.getVerticalScrollBar().setBackground(DarkPalette.BG.getColor());
+		
+		// Films manager.
+		this.manager.setDark();
 	}
 
 }
