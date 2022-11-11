8f8a3a77157dededaf37c2108753839b1baa15fa
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-11 23:38:08 +0100

    refactor: remove abstraction for QRCode
    Every film is available as a QRCode

--- Development/src/main/java/fc/Films/Film.java
+++ Development/src/main/java/fc/Films/Film.java
@@ -1,11 +1,27 @@
 package fc.Films;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 /**
  * Film is an abstract class that can implements the bluray class and the QRcode class
  * @author Clarisse
  *
  */
-public abstract class Film {
+public class Film {
 
 	protected String title;
 	protected String synopsis;
@@ -39,9 +55,11 @@ public abstract class Film {
 	
 	/**
 	 * Allows to know the film support
-	 * @return the film support "BluRay" or "QRCode"
+	 * @return the film support "QRCode" only
 	 */
-	public abstract String getType();
+	public String getType() { 
+		return "QRCode";
+	}
 
 	/**
 	 * 
@@ -91,15 +109,53 @@ public abstract class Film {
 	}
 
 	/**
+	 * Generate the QRCode of the link of the film
+	 * Create a file QRCode.png with the QRCode
+	 * @return the link of the film
 	 * 
 	 * @return the categories of the film
+	 * @see Ressources generator : https://www.digitalocean.com/community/tutorials/java-qr-code-generator-zxing-example
 	 */
-	public String getCategories() {
-		String categoryList="";
-		for (int i=0;i<categories.length-1;i++) {
-			categoryList+=categories[i]+", ";
+	public String generateQRCode() {
+		String link = "https://www.cybervideo/location/"+this.getTitle()+".com";
+
+		try {
+		String filePath = "QRCode.png";
+		int size = 125;
+		String fileType = "png";
+		File qrFile = new File(filePath);
+		// Create the ByteMatrix for the QR-Code that encodes the given String
+		Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		BitMatrix byteMatrix = null;
+		byteMatrix = qrCodeWriter.encode(link, BarcodeFormat.QR_CODE, size, size, hintMap);
+		// Make the BufferedImage that are to hold the QRCode
+		int matrixWidth = byteMatrix.getWidth();
+		BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+		image.createGraphics();
+
+		Graphics2D graphics = (Graphics2D) image.getGraphics();
+		graphics.setColor(Color.WHITE);
+		graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+		// Paint and save the image using the ByteMatrix
+		graphics.setColor(Color.BLACK);
+
+		for (int i = 0; i < matrixWidth; i++) {
+			for (int j = 0; j < matrixWidth; j++) {
+				if (byteMatrix.get(i, j)) {
+					graphics.fillRect(i, j, 1, 1);
+				}
+			}
 		}
-		categoryList+=categories[categories.length-1];
-		return categoryList;
-	}	
+		ImageIO.write(image, fileType, qrFile);
+
+		
+		} catch (IOException | WriterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return link;
+	}
+	
 }
