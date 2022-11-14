b1b1f97664a740cff05a57bcaad8625393b477bc
Ayoub Baladi <baladi230@gmail.com>
2022-11-14 20:03:41 +0100

    feat: AL2000 base fc

--- Development/src/main/java/fc/AL2000.java
+++ Development/src/main/java/fc/AL2000.java
@@ -2,16 +2,49 @@ package fc;
 
 import fc.Films.BluRay;
 import fc.Films.StatesBluRay;
+import fc.Films.Film;
 
+import java.sql.*;
+import java.util.ArrayList;
 
 public class AL2000 {
+	String db;
+	String userName;
+	String pw;
+	ArrayList<Film> filmList;
+	//Film attrs
+	String title, synopsis, directorLastName, directorFirstName, restrictedAge;
+	int filmID;
+	ArrayList<String> actors;
+
+	public AL2000(){
+		db = "";
+		userName = "";
+		pw = "";
+		filmList = new ArrayList<String>();
+		actors = new ArrayList<String>();
+
+		try{
+			Connection conn = DriverManager.getConnection();
+			String sqlReq = "SELECT * FROM Films;";
+			Statement stm = conn.createStatement();
+			ResultSet res = stm.executeQuery(sqlReq);
+
+			while (res.next()){
+				//Get db informations
+			}
+		}
+		catch(SQLException e){
+			System.out.println(e.getSQLState());
+		}
+	}
 
 	public boolean collectBluRay(BluRay film) {
 		if(checkIDBluRay(film))
 			if(film.getState().equals(StatesBluRay.RENTED)) {
-				if(showBluRayDamagedDiolog()) 
+				if(showBluRayDamagedDiolog())
 					film.setState(StatesBluRay.DAMAGED);
-				else 
+				else
 					film.setState(StatesBluRay.AVAILABLE);
 				// CODE : Si le client a bien payé ses retards, si sa situation est reglementé
 				// IHM : Afficher un message comme quoi le BluRay a bien été rendu
@@ -22,7 +55,7 @@ public class AL2000 {
 	}
 
 	private static boolean checkIDBluRay(BluRay film) {
-		// BDD : si ID corespond ID de la base 		
+		// BDD : si ID corespond ID de la base
 		return true;
 	}
 
@@ -34,7 +67,7 @@ public class AL2000 {
 				return true;
 			}
 		// IHM : Afficher un message comme quoi le BluRay ne peut être distribué
-		return false;	
+		return false;
 	}
 
 	//	public String printReceipt(Rented location) {
@@ -48,7 +81,7 @@ public class AL2000 {
 	//	}
 
 	public void printQRCode(){
-		
+
 	}
 
 	private boolean showBluRayDamagedDiolog() {
