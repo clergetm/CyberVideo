4b5962d5999b3f5104919178f09d082e2b136653
Evan1700 <evan17.pereira@gmail.com>
2022-11-15 17:58:12 +0100

    feat: add the main abstract DAO class

---
+++ Development/src/main/java/bd/DAO_Classes/MainDAO.java
@@ -0,0 +1,42 @@
+package bd.DAO_Classes;
+
+import java.sql.Connection;
+
+/**
+ * @author EvanP
+ * Class that initializes the DAO to link database and functional core.
+ */
+public abstract class MainDAO<T> {
+    
+    protected Connection connect = null;
+
+    public MainDAO(Connection conn)
+    { 
+        this.connect = conn; 
+    } 
+
+    /**
+     * Read an object with his ID
+     * @param id
+     * @return
+     */
+    public abstract T read(int id);
+    
+    /**
+     * Create entry in database
+     * @param obj
+     */
+    public abstract boolean create(T obj);
+    
+    /**
+     * Update entry in database
+     * @param obj
+     */
+    public abstract boolean update(T obj);
+    
+    /**
+     * Delete entry in database
+     * @param obj
+     */
+    public abstract boolean delete(T obj);
+}
