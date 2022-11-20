d20b2f51a0bb02261c071396fabfac88ed8cdeb2
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-20 15:42:49 +0100

    feat: Base concrete CartPanel class

--- Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmCartPanel.java
+++ Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmCartPanel.java
@@ -1,10 +1,32 @@
 package ui.utils.factory.filmpanel.factories;
 
+import fc.films.Film;
+import ui.utils.factory.filmpanel.products.CartButton;
+import ui.utils.factory.filmpanel.products.FilmPanelButton;
+
 /**
  * Factory to create FilmCartPanel.
  * @author MathysC
  *
  */
+@SuppressWarnings("serial")
 public class FilmCartPanel extends FilmPanel {
+
+    protected FilmCartPanel(Film film) {
+	super(film);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected FilmPanelButton createButton(String supportType) {
+	CartButton button = new CartButton();
+	button.setText(supportType);
+	button.setName(supportType);
+	button.setFont(font);
+	button.setEnabled(false); // Disabled by default.
+	
+	this.buttonPanel.add(button); // Add to the panel 
+	return button;
+    }
 
 }
