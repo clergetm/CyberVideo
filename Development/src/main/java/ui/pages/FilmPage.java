86a80c907f24644bc71669b3b8b8184c1d8ee65d
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-20 11:36:04 +0100

    fix(FIXME): filmPage to fix

--- Development/src/main/java/ui/pages/FilmPage.java
+++ Development/src/main/java/ui/pages/FilmPage.java
@@ -38,53 +38,53 @@ public class FilmPage extends JPanel implements ColorTheme {
     JTextArea actorsText = new JTextArea(50,50);
 
 
-
+//FIXME
     public FilmPage(){
-
-	    JPanel tempFilmPoster = new JPanel();
-	    tempFilmPoster.setLayout(new BoxLayout(tempFilmPoster,BoxLayout.Y_AXIS));
-	    tempFilmPoster.setOpaque(false);
-
-        this.filmPoster = new FilmPanel(this.film, 200, false);
-        
-        tempFilmPoster.add( Box.createVerticalGlue());
-        tempFilmPoster.add(filmPoster);
-        tempFilmPoster.add( Box.createVerticalGlue());
-        this.setBackground(Color.CYAN);
-        this.setLayout(new BorderLayout());
-
-       //Creating a container to place the actual JPanel filmInfo
-       filmInfoContainer.setLayout(new FlowLayout(FlowLayout.LEFT,0,30));
-       filmInfoContainer.setOpaque(false);
-       filmInfo.setOpaque(false);
-
-       //Creating the filmInfo JPanel
-       //Setting Layout and size 
-        filmInfo.setLayout(new GridLayout( 0,1 ));
-        filmInfo.setPreferredSize(new Dimension(500, 500));
-        filmInfo.setMaximumSize(new Dimension(500, 500));
-
-        //creating JLabel MovieName
-        setTitle(this.movieName,(this.film.getTitle()),filmInfo,26);
-        
-
-        //Creating JLabel and textArea for the synopsis
-        setTitle(this.synopsis,"Synopsis",filmInfo,20);
-        setTextArea(synopsisText,filmInfo,this.film.getSynopsis());
-
-        //Creating JLabel and textArea for the directors
-        setTitle(this.directors,"Directors",filmInfo,20);
-        setTextArea(directorsText,filmInfo,this.film.getNamesDirector());
-
-        //Creating JLabel and textArea for the actors
-        this.actorsTest = String.join(", ",this.film.getActors()); //Converts array of String to a single String
-        setTitle(this.actors,"Actors",filmInfo,20);
-        setTextArea(actorsText,filmInfo,actorsTest);
-        
-        //Adding elements to Jpanels
-        filmInfoContainer.add(filmInfo);
-        this.add(filmInfoContainer, BorderLayout.CENTER);
-        this.add(tempFilmPoster, BorderLayout.WEST);
+//
+//	JPanel tempFilmPoster = new JPanel();
+//	tempFilmPoster.setLayout(new BoxLayout(tempFilmPoster,BoxLayout.Y_AXIS));
+//	tempFilmPoster.setOpaque(false);
+//
+//        this.filmPoster = new FilmPanel(this.film, 200, false);
+//        
+//        tempFilmPoster.add( Box.createVerticalGlue());
+//        tempFilmPoster.add(filmPoster);
+//        tempFilmPoster.add( Box.createVerticalGlue());
+//        this.setBackground(Color.CYAN);
+//        this.setLayout(new BorderLayout());
+//
+//       //Creating a container to place the actual JPanel filmInfo
+//       filmInfoContainer.setLayout(new FlowLayout(FlowLayout.LEFT,0,30));
+//       filmInfoContainer.setOpaque(false);
+//       filmInfo.setOpaque(false);
+//
+//       //Creating the filmInfo JPanel
+//       //Setting Layout and size 
+//        filmInfo.setLayout(new GridLayout( 0,1 ));
+//        filmInfo.setPreferredSize(new Dimension(500, 500));
+//        filmInfo.setMaximumSize(new Dimension(500, 500));
+//
+//        //creating JLabel MovieName
+//        setTitle(this.movieName,(this.film.getTitle()),filmInfo,26);
+//        
+//
+//        //Creating JLabel and textArea for the synopsis
+//        setTitle(this.synopsis,"Synopsis",filmInfo,20);
+//        setTextArea(synopsisText,filmInfo,this.film.getSynopsis());
+//
+//        //Creating JLabel and textArea for the directors
+//        setTitle(this.directors,"Directors",filmInfo,20);
+//        setTextArea(directorsText,filmInfo,this.film.getNamesDirector());
+//
+//        //Creating JLabel and textArea for the actors
+//        this.actorsTest = String.join(", ",this.film.getActors()); //Converts array of String to a single String
+//        setTitle(this.actors,"Actors",filmInfo,20);
+//        setTextArea(actorsText,filmInfo,actorsTest);
+//        
+//        //Adding elements to Jpanels
+//        filmInfoContainer.add(filmInfo);
+//        this.add(filmInfoContainer, BorderLayout.CENTER);
+//        this.add(tempFilmPoster, BorderLayout.WEST);
         
     }   
    
@@ -116,39 +116,39 @@ public class FilmPage extends JPanel implements ColorTheme {
 
     @Override
     public void setLight() {
-	    this.filmPoster.setLight();
-        // This JPanel
-        this.setBackground(Light.BG.getColor());
-        //JLabel movieName, synopsis, directors, actors;
-        this.movieName.setForeground(Light.BLACK.getColor());
-        this.synopsis.setForeground(Light.BLACK.getColor());
-        this.directors.setForeground(Light.BLACK.getColor());
-        this.actors.setForeground(Light.BLACK.getColor());
-
-        //JTextAreas
-        this.synopsisText.setForeground(Light.BLACK.getColor());
-        this.directorsText.setForeground(Light.BLACK.getColor());
-        this.actorsText.setForeground(Light.BLACK.getColor());
+//	    this.filmPoster.setLight();
+//        // This JPanel
+//        this.setBackground(Light.BG.getColor());
+//        //JLabel movieName, synopsis, directors, actors;
+//        this.movieName.setForeground(Light.BLACK.getColor());
+//        this.synopsis.setForeground(Light.BLACK.getColor());
+//        this.directors.setForeground(Light.BLACK.getColor());
+//        this.actors.setForeground(Light.BLACK.getColor());
+//
+//        //JTextAreas
+//        this.synopsisText.setForeground(Light.BLACK.getColor());
+//        this.directorsText.setForeground(Light.BLACK.getColor());
+//        this.actorsText.setForeground(Light.BLACK.getColor());
 
     }
 
 
     @Override
     public void setDark() {
-	    this.filmPoster.setDark();
-        // This JPanel
-        this.setBackground(Dark.BG.getColor());
-        //JLabel movieName, synopsis, directors, actors;
-        this.movieName.setForeground(Dark.FOREGROUND.getColor());
-        this.synopsis.setForeground(Dark.FOREGROUND.getColor());
-        this.directors.setForeground(Dark.FOREGROUND.getColor());
-        this.actors.setForeground(Dark.FOREGROUND.getColor());
-
-        //JTextAreas
-        this.synopsisText.setForeground(Dark.FOREGROUND.getColor());
-        this.directorsText.setForeground(Dark.FOREGROUND.getColor());
-        this.actorsText.setForeground(Dark.FOREGROUND.getColor());
-	
+//	    this.filmPoster.setDark();
+//        // This JPanel
+//        this.setBackground(Dark.BG.getColor());
+//        //JLabel movieName, synopsis, directors, actors;
+//        this.movieName.setForeground(Dark.FOREGROUND.getColor());
+//        this.synopsis.setForeground(Dark.FOREGROUND.getColor());
+//        this.directors.setForeground(Dark.FOREGROUND.getColor());
+//        this.actors.setForeground(Dark.FOREGROUND.getColor());
+//
+//        //JTextAreas
+//        this.synopsisText.setForeground(Dark.FOREGROUND.getColor());
+//        this.directorsText.setForeground(Dark.FOREGROUND.getColor());
+//        this.actorsText.setForeground(Dark.FOREGROUND.getColor());
+//	
     }
 
 }
