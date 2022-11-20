728724caa5026fbf8ca8a247aa56c18c87dddbfd
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-20 15:42:57 +0100

    feat: Base concrete RentPanel class

--- Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmRentPanel.java
+++ Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmRentPanel.java
@@ -1,10 +1,32 @@
 package ui.utils.factory.filmpanel.factories;
 
+import fc.films.Film;
+import ui.utils.factory.filmpanel.products.FilmPanelButton;
+import ui.utils.factory.filmpanel.products.RentButton;
+
 /**
  * Factory to create FilmRentPanel.
  * @author MathysC
  *
  */
+@SuppressWarnings("serial")
 public class FilmRentPanel extends FilmPanel {
+
+    protected FilmRentPanel(Film film) {
+	super(film);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected FilmPanelButton createButton(String supportType) {
+	RentButton button = new RentButton();
+	button.setText(supportType);
+	button.setName(supportType);
+	button.setFont(font);
+	button.setEnabled(false); // Disabled by default.
+	
+	this.buttonPanel.add(button); // Add to the panel 
+	return button;
+    }
 
 }
