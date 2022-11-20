d8e9e1778aedb686bd23a62d822229a6ca02b312
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-20 18:15:05 +0100

    feat: CartButton implementation CartComponent

--- Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmCartPanel.java
+++ Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmCartPanel.java
@@ -19,7 +19,7 @@ public class FilmCartPanel extends FilmPanel {
 
     @Override
     protected FilmPanelButton createButton(String supportType) {
-	CartButton button = new CartButton();
+	CartButton button = new CartButton(this.getFilm(), supportType);
 	button.setText(supportType);
 	button.setName(supportType);
 	button.setFont(font);
--- Development/src/main/java/ui/utils/factory/filmpanel/products/CartButton.java
+++ Development/src/main/java/ui/utils/factory/filmpanel/products/CartButton.java
@@ -1,11 +1,51 @@
 package ui.utils.factory.filmpanel.products;
 
+import java.awt.event.ActionEvent;
+
+import fc.films.Film;
+import ui.utils.mediator.cart.CartMediator;
+import ui.utils.mediator.cart.components.CartComponent;
+
 /**
  * Concrete FilmPanelButton implementation to add a film to the cart.
  * @author MathysC
  *
  */
 @SuppressWarnings("serial")
-public class CartButton extends FilmPanelButton {
+public class CartButton extends FilmPanelButton implements CartComponent {
+    private CartMediator cartMediator;
+    private Film film;
+    private String supportType;
+    
+    /**
+     * Constructor of CartButton
+     * @author MathysC
+     *
+     * @param film The film to add to the cart.
+     * @param supportType the way film is supported. (e.g. Blu-Ray, QR Code)
+     */
+    public CartButton(Film film, String supportType) {
+	this.film = film;
+	this.supportType = supportType;
+    }
+    
+    @Override
+    public void setMediator(CartMediator cartMediator) {
+	this.cartMediator = cartMediator;
+    }
 
+    /**
+     * When this button is clicked, The mediator is used in order to add a film to the cart.
+     * @author MathysC
+     *
+     */
+    @Override
+    protected void fireActionPerformed(ActionEvent actionEvent) {
+	this.cartMediator.addToCart(this.film, this.supportType);
+    }
+
+    @Override
+    public String getComponentName() {
+	return "CartButton";
+    }
 }
