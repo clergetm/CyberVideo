62802abcc60d5e36e233193975015a40d73ec186
Evan Pereira <evan17.pereira@gmail.com>
2022-11-21 15:08:15 +0100

    fix: add updated fc

--- Development/src/test/java/fc/Films/BluRayTest.java
+++ Development/src/test/java/fc/Films/BluRayTest.java
@@ -3,86 +3,57 @@ package fc.Films;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import fc.films.BluRay;
+import fc.films.StatesBluRay;
 
 class BluRayTest {
 
-	/**
-	 * Create an instance of a BluRay for test BluRay class
-	 */
-	protected static BluRay bluRay = new BluRay(22.90, StatesBluRay.AVAILABLE, "tata", "tata tutu toto", new String[]{"JM POPO","PE RORO"}, "DUDU", "DADA", AgeRestriction.EVERYONE, new Categories[] {Categories.ANIME});
-
-	/**
-	 * Check getType()
-	 */
+	/* Create Film attributes */
+	private final double price = 20;
+	private final StatesBluRay state = StatesBluRay.AVAILABLE;
+	
+	/* Create an instance of a BluRay for test BluRay class */
+	protected BluRay bluRay = new BluRay(price, state);
+	
+	
 	@Test
 	void testGetType() {
-		// Test will pass
-		Assertions.assertEquals("BluRay", bluRay.getType(), "bluRay.getType() test passed");
-		
-		//Test will fail 
-	    //Assertions.assertEquals("QRCode", bluRay.getType(), "bluRay.getType() test failed");
-	    Assertions.assertNotEquals("QRCode", bluRay.getType(), "bluRay.getType() test failed");
-
+		Assertions.assertEquals("BluRay", bluRay.getType());
+	    Assertions.assertNotEquals("QRCode", bluRay.getType());
 	}
 
-	/**
-	 * Check the constructor of BluRay
-	 */
+	@Test
+	void testGetTariff() {
+	    Assertions.assertEquals(5, bluRay.getTariff());
+	}
+
+	@Test
+	void testIsAvailable() {
+		Assertions.assertTrue(bluRay.isAvailable());
+	}
+
 	@Test
 	void testBluRay() {
-		Assertions.assertEquals("tata", bluRay.title, "bluRay.title test passed");
-		Assertions.assertEquals("tata tutu toto", bluRay.synopsis, "bluRay.synopsis test passed");
-		Assertions.assertEquals("JM POPO, PE RORO", bluRay.getActors(), "bluRay.getActors() test passed");
-		Assertions.assertEquals("DUDU", bluRay.FNameDirector, "bluRay.FNameDirector test passed");
-		Assertions.assertEquals("DADA", bluRay.LNameDirector, "bluRay.LNameDirector test passed");
-		Assertions.assertEquals("ANIME", bluRay.getCategories(), "bluRay.getCategories() test passed");
-		Assertions.assertEquals(AgeRestriction.EVERYONE, bluRay.restriction, "bluRay.restriction test passed");
+		Assertions.assertEquals(price, bluRay.getPurchasePrice());
+		Assertions.assertEquals(state, StatesBluRay.AVAILABLE);	
 	}
 
-	/**
-	 * Check getPrice()
-	 */
 	@Test
-	void testGetPrice() {
-		Assertions.assertEquals(22.90, bluRay.getPrice(), "bluRay.getPrice() test passed");
+	void testGetPurchasePrice() {
+		Assertions.assertEquals(price, bluRay.getPurchasePrice());
 	}
 
-	/**
-	 * Check setPrice
-	 */
-	@Test
-	void testSetPrice() {
-		bluRay.setPrice(24.57);
-		
-		// Test will pass
-		Assertions.assertEquals(24.57, bluRay.getPrice(), "bluRay.setPrice() test passed");
-		
-		//Test will fail 
-		//Assertions.assertEquals(22.90, bluRay.getPrice(), "bluRay.setPrice() test failed");
-		Assertions.assertNotEquals(22.90, bluRay.getPrice(), "bluRay.setPrice() test failed");
-	}
-	
-	/**
-	 * Check getState()
-	 */
 	@Test
 	void testGetState() {
-		// Test will pass
-		Assertions.assertEquals(StatesBluRay.AVAILABLE, bluRay.getState(), "bluRay.getState() test passed");
+		Assertions.assertEquals(state, bluRay.getState());
 	}
 
-	/**
-	 * Check setStat()
-	 */
 	@Test
 	void testSetState() {
-		bluRay.setState(StatesBluRay.STOLEN);
-		
-		// Test will pass
-		Assertions.assertEquals(StatesBluRay.STOLEN, bluRay.getState(), "bluRay.setState() test passed");
-		
-		//Test will fail 
-		//Assertions.assertEquals(StatesBluRay.AVAILABLE, bluRay.getState(), "bluRay.setState() test failed");
-		Assertions.assertNotEquals(StatesBluRay.AVAILABLE, bluRay.getState(), "bluRay.setState() test failed");
+		bluRay.setState(StatesBluRay.RENTED);
+		Assertions.assertEquals(StatesBluRay.RENTED, bluRay.getState());
+		Assertions.assertNotEquals(StatesBluRay.AVAILABLE, bluRay.getState());
+		bluRay.setState(StatesBluRay.AVAILABLE);
+
 	}
 }
--- Development/src/test/java/fc/Films/FilmTest.java
+++ Development/src/test/java/fc/Films/FilmTest.java
@@ -1,84 +1,91 @@
 package fc.Films;
 
+import java.time.Year;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import fc.LifecycleLoggerTest;
+import fc.films.AgeRestriction;
+import fc.films.BluRay;
+import fc.films.Categories;
+import fc.films.Film;
+import fc.films.QRCode;
+import fc.films.StatesBluRay;
+import fc.films.Support;
 
-class FilmTest implements LifecycleLoggerTest {
+class FilmTest implements LifecycleLoggerTest{
+
 	
-	/**
-	 * Create an instance of a Film for test Film class
-	 */
-	protected static Film film = new QRCode("toto", "toto tutu tata", new String[]{"JM COCO","PE SOSO"}, "DIDI", "DODO", AgeRestriction.MINUS12, new Categories[] {Categories.DRAMAS, Categories.COMEDIES});
+	/* Create Film attributes */
+	private final String title = "The Matrix",
+			synopsis = "Thomas A. Anderson is a man living two lives. "
+					+ "By day he is an average computer programmer and by night "
+					+ "a hacker known as Neo. Neo has always questioned his reality,"
+					+ " but the truth is far beyond his imagination...",
+			director_lname = "Wachowski",
+			director_fname = "Lana";
+	private final String[] actors = {"Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving"};
+	private final AgeRestriction restriction = AgeRestriction.EVERYONE;
+	private final Categories[] categories = {Categories.ACTION, Categories.DRAMAS};
+	private final Year year = Year.of(1999);
+	private final Support[] supports = {new BluRay(22.5, StatesBluRay.RENTED), new QRCode(), new BluRay(22.50, StatesBluRay.AVAILABLE)};
 	
-	/**
-	 * Check the constructor of Film
-	 */
+	/* Create an instance of a Film for test Film class */
+	protected Film film = new Film(title, synopsis, actors, director_fname, director_lname, year, categories, restriction, supports);
+	
+	
 	@Test
 	void testFilm() {
-		Assertions.assertEquals("toto", film.title, "film.title test passed");
-		Assertions.assertEquals("toto tutu tata", film.synopsis, "film.synopsis test passed");
-		Assertions.assertEquals("JM COCO, PE SOSO", film.getActors(), "film.getActors() test passed");
-		Assertions.assertEquals("DIDI", film.FNameDirector, "film.FNameDirector test passed");
-		Assertions.assertEquals("DODO", film.LNameDirector, "film.LNameDirector test passed");
-		Assertions.assertEquals("DRAMAS, COMEDIES", film.getCategories(), "film.getCategories() test passed");
-		Assertions.assertEquals(AgeRestriction.MINUS12, film.restriction, "film.restriction test passed");
+		Assertions.assertEquals(title, film.getTitle());
+		Assertions.assertEquals(synopsis, film.getSynopsis());
+		Assertions.assertEquals(actors, film.getActors());
+		Assertions.assertEquals(director_fname, film.getNamesDirector().substring(0, film.getNamesDirector().indexOf(" ")));
+		Assertions.assertEquals(director_lname, film.getNamesDirector().substring(film.getNamesDirector().indexOf(" ")+1));
+		Assertions.assertEquals(year, film.getYear());
+		Assertions.assertEquals(categories, film.getCategories());
+		Assertions.assertEquals(restriction, film.getRestriction());
+		Assertions.assertEquals(supports, film.getSupports());
 	}
 
-	/**
-	 * Check getType()
-	 */
-	@Test
-	void testGetType() {
-		Assertions.assertEquals("QRCode", film.getType(), "film.getType() test passed");
-	}
-
-	/**
-	 * Check getTitle()
-	 */
 	@Test
 	void testGetTitle() {
-		Assertions.assertEquals("toto", film.getTitle());
+		Assertions.assertEquals(title, film.getTitle());
 	}
 
-	/**
-	 * Check getSynopsis()
-	 */
 	@Test
 	void testGetSynopsis() {
-		Assertions.assertEquals("toto tutu tata", film.getSynopsis());
+		Assertions.assertEquals(synopsis, film.getSynopsis());
 	}
 
-	/**
-	 * Check getActors()
-	 */
 	@Test
 	void testGetActors() {
-		Assertions.assertEquals("JM COCO, PE SOSO", film.getActors());
+		Assertions.assertEquals(actors, film.getActors());
 	}
 
-	/**
-	 * Check getNamesDirector()
-	 */
 	@Test
 	void testGetNamesDirector() {
-		Assertions.assertEquals("DIDI DODO", film.getNamesDirector());
+		Assertions.assertEquals(director_fname+" "+director_lname, film.getNamesDirector());
 	}
 
-	/**
-	 * Check getRestriction()
-	 */
 	@Test
-	void testGetRestriction() {
-		Assertions.assertEquals(AgeRestriction.MINUS12, film.getRestriction());
+	void testGetYear() {
+		Assertions.assertEquals(year, film.getYear());
 	}
 
-	/**
-	 * Check getCategories()
-	 */
 	@Test
 	void testGetCategories() {
-		Assertions.assertEquals("DRAMAS, COMEDIES", film.getCategories());
+		Assertions.assertEquals(categories, film.getCategories());
 	}
+
+	@Test
+	final void testGetRestriction() {
+		Assertions.assertEquals(restriction, film.getRestriction());
+	}
+
+	@Test
+	void testGetSupportsType() {
+		Assertions.assertEquals(supports, film.getSupports());
+	}
+
 }
