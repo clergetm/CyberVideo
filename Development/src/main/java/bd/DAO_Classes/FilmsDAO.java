72b750b58f4f2e662e96b6cabcfbd251678cb3a9
Evan Pereira <evan17.pereira@gmail.com>
2022-11-21 13:59:56 +0100

    feat: add Films DAO class

---
+++ Development/src/main/java/bd/DAO_Classes/FilmsDAO.java
@@ -0,0 +1,75 @@
+package bd.DAO_Classes;
+
+import fc.Films.AgeRestriction;
+import fc.Films.Categories;
+import fc.Films.Film;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class FilmsDAO<Film> extends DAO<Film>{
+    
+    public FilmsDAO(Connection conn)
+    {
+        super(conn);
+    }
+
+    @Override
+    public Film read(int id) {
+        
+        try{
+
+            ResultSet result = this.connect.createStatement().executeQuery("SELECT * FROM Films JOIN FilmCategories WHERE filmID ="+id);
+            ResultSet result2 = this.connect.createStatement().executeQuery("SELECT CONCAT(actorFirstName,' ',actorLastName) AS actorName FROM FilmsActors NATURAL JOIN Actors WHERE filmID ="+id);
+            ResultSet result3 = this.connect.createStatement().executeQuery("SELECT catName FROM FilmsCategories NATURAL JOIN Categories WHERE filmID ="+id);
+            
+            if(result.first()){
+                String title = result.getString("title");
+
+                String synopsis = result.getString("synopsis");
+
+                ArrayList<String> actors = new ArrayList<String>();
+                while(result2.next()){
+                    actors.add(result2.getString("actorName"));
+                }
+
+                String FNDirector = result.getString("directorFirstName");
+
+			    String LNDirector = result.getString("directorLastName");
+
+                AgeRestriction restriction = AgeRestriction.valueOf(result.getString("restrictedAge"));
+
+                ArrayList<String> categories = new ArrayList<String>();
+                while(result2.next()){
+                    categories.add(result3.getString("actorName"));
+                }
+
+
+                Film film = new Film(title,synopsis,actors,FNDirector,LNDirector,restriction,categories);
+            }
+            
+        }
+        catch (SQLException e) { e.printStackTrace(); }
+        
+        return film;
+    }
+
+    @Override
+    public boolean create(Film obj) {
+        
+        return false;
+    }
+
+    @Override
+    public boolean update(Film obj) {
+        
+        return false;
+    }
+
+    @Override
+    public boolean delete(Film obj) {
+        
+        return false;
+    }
+}
