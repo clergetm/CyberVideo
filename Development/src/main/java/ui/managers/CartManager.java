53b97072e96af0da7611cb273d095fff09f22008
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-22 22:20:01 +0100

    feat: CartManager, implements Observable

---
+++ Development/src/main/java/ui/managers/CartManager.java
@@ -0,0 +1,75 @@
+package ui.managers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import fc.films.Film;
+import ui.utils.factory.filmpanel.products.CartButton;
+import ui.utils.observe.cart.IObservable;
+import ui.utils.observe.cart.IObserver;
+
+public class CartManager implements IObservable, ActionListener{
+ 
+    private ArrayList<IObserver> observers;
+    
+    /**
+     * Default constructor of {@code CartManager}.
+     * @author MathysC
+     */
+    public CartManager() {
+	this.observers = new ArrayList<>();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	FilmEvents event = FilmEvents.getFromName(e.getActionCommand());
+	switch (event){
+	case ADDTOCART:
+//	    TODO TOCART
+//	    TODO récupérer le film et le supportType
+	    CartButton source = (CartButton)e.getSource();
+	    this.notifyObservers(FilmEvents.ADDTOCART, source.getFilm(), source.getSupportType());
+	    break;
+	case REMOVEFROMCART:
+//	    TODO REMOVE
+	    break;
+	case RENT:
+//	    TODO RENT
+//	    TODO récupérer le film et le supportType
+	    this.notifyObservers(null, null, null);
+	    break;
+	default:
+	    throw new IllegalArgumentException("Unexpected value: " + e.getActionCommand());
+	}
+	
+    }
+
+    @Override
+    public void register(IObserver observer) {
+	this.observers.add(observer);
+	
+    }
+
+    @Override
+    public void unregister(IObserver observer) {
+	this.observers.remove(observer);
+	
+    }
+
+    @Override
+    public void notifyObservers(FilmEvents event, Film film, String supportType) {
+	switch (event) {
+	case ADDTOCART:
+	    this.observers.forEach(o -> o.addToCart(film, supportType));
+	    break;
+	case REMOVEFROMCART:
+	    this.observers.forEach(o -> o.removeFromCart(film, supportType));
+	    break;
+	default:
+	    throw new IllegalArgumentException("Unexpected value: " + event);
+	}
+	
+    }
+
+}
