216a60b9dc03f0c34debc2cbd363a8b50a6b205e
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-22 18:10:43 +0100

    feat: Enum of Cart events

---
+++ Development/src/main/java/ui/utils/observe/cart/CartEvent.java
@@ -0,0 +1,11 @@
+package ui.utils.observe.cart;
+
+/**
+ * The enumeration of events related to the cart.
+ * @author MathysC
+ *
+ */
+public enum CartEvent {
+    ADD,	// Add a Film to the cart.
+    REMOVE;	// Remove a Film from the cart.
+}
