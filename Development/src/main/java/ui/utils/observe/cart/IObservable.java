be7505ceff68c2f0ab62c74be8786aba6d9dc5eb
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-22 18:07:07 +0100

    feat: IObservable

---
+++ Development/src/main/java/ui/utils/observe/cart/IObservable.java
@@ -0,0 +1,36 @@
+package ui.utils.observe.cart;
+
+import fc.films.Film;
+
+/**
+ * Common Interface for Observable cart related object.
+ * @author MathysC
+ */
+public interface IObservable {
+
+    /**
+     * Add an observer.
+     * @author MathysC
+     *
+     * @param observer The observer to register.
+     */
+    void register(IObserver observer);
+    
+    /**
+     * Remove an observer.
+     * @author MathysC
+     *
+     * @param observer The observer to unregister.
+     */
+    void unregister(IObserver observer);
+    
+    /**
+     * Notify observers of any change depending on the Event
+     * @author MathysC
+     *
+     * @param event The type of event (ADD or REMOVE).
+     * @param film The film related to this change.
+     * @param supportType The supportType related to this Film.
+     */
+    void notifyObservers(CartEvent event, Film film, String supportType);
+}
