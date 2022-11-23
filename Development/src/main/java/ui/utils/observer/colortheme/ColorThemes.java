36a5bcdb6ee1cdc2bf7c8c9a1f628f2e4504e18a
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-23 13:27:29 +0100

    feat: enumeration of all color themes

---
+++ Development/src/main/java/ui/utils/observer/colortheme/ColorThemes.java
@@ -0,0 +1,14 @@
+package ui.utils.observer.colortheme;
+
+/**
+ * Enumeration of all color Theme.
+ * @author MathysC
+ * @see ui.utils.observer.colortheme.IColorThemeObserver
+ * @see ui.utils.observer.colortheme.IColorThemeObservable
+ * @see ui.utils.observer.colortheme.palettes.Dark
+ * @see ui.utils.observer.colortheme.palettes.Light
+ */
+public enum ColorThemes {
+    LIGHTTHEME,
+    DARKTHEME;
+}
