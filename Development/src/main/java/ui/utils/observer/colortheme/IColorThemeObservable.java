ff289fc0531b2b20bbefe41110d26c3d7c19b6d2
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-23 13:24:47 +0100

    feat: colortheme observable object

---
+++ Development/src/main/java/ui/utils/observer/colortheme/IColorThemeObservable.java
@@ -0,0 +1,36 @@
+package ui.utils.observer.colortheme;
+
+/**
+ * Common interface for Color Theme observable objects.
+ * @author MathysC
+ * @see ui.utils.observer.colortheme.IColorThemeObserver
+ * @see ui.utils.observer.colortheme.ColorThemes
+ * @see ui.utils.observer.colortheme.palettes.Dark
+ * @see ui.utils.observer.colortheme.palettes.Light
+ */
+public interface IColorThemeObservable {
+
+    /**
+     * Add an observer.
+     * @author MathysC
+     *
+     * @param colorThemeObserver the observer.
+     */
+    void register(IColorThemeObserver colorThemeObserver);
+    
+    /**
+     * Remove an observer.
+     * @author MathysC
+     *
+     * @param colorThemeObserver the observer.
+     */
+    void unregister(IColorThemeObserver colorThemeObserver);
+    
+    /**
+     * Change color theme of all observers.
+     * @author MathysC
+     *
+     * @param color the color to set.
+     */
+    void notifyObservers(ColorThemes color);
+}
