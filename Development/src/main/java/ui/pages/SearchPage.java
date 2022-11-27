4e1b55452ef8505628a5239d4f45776daaedd329
Clerget Mathys <mathysclerget+github@gmail.com>
2022-11-27 22:26:25 +0100

    fix: Searchpage #42

--- Development/src/main/java/ui/pages/SearchPage.java
+++ Development/src/main/java/ui/pages/SearchPage.java
@@ -17,6 +17,7 @@ import javax.swing.ScrollPaneConstants;
 
 import fc.films.Categories;
 import fc.films.Film;
+import ui.GUIComponent;
 import ui.managers.FilmManager;
 import ui.managers.GUIManager;
 import ui.managers.panels.FilmManagerPanel;
@@ -29,145 +30,163 @@ import ui.utils.observer.colortheme.palettes.Light;
 import ui.utils.observer.multilingual.IMultilingualObserver;
 
 @SuppressWarnings("serial")
-public class SearchPage extends JPanel implements IMultilingualObserver, IColorThemeObserver {
-
-    	protected JPanel mainPanel = new JPanel(new BorderLayout());
+public class SearchPage extends JPanel implements GUIComponent, IMultilingualObserver, IColorThemeObserver {
     	
-    	/* Results */
-    	protected JPanel resultsPanel = new JPanel(new BorderLayout());
-	protected JLabel resultsLabel = new JLabel();
-    	protected JScrollPane resultsPane;
-    	private FilmManagerPanel resultsManager;
-    	private int countResults = 0;
-    	
-    	/* Most rented films */
-    	protected JPanel mostRentedPanel = new JPanel(new BorderLayout());
-    	protected JLabel mostRentedLabel = new JLabel();
-    	protected JScrollPane mostRentedPane;
-    	private FilmManagerPanel mostRentedManager;
-    	
-    	/* Categories */
-    	protected JScrollPane categoriesPane;
-    	private JPanel categoriesManager;
-    	
-    	/* Film Manager */
-    	private FilmManager filmManager;
-    	/**
-    	 * 
-    	 * @author MathysC
-    	 *
-    	 */
-	public SearchPage() {
-	    this.setLayout(new BorderLayout());
-	    resultsPanel.setOpaque(false);
-	    mostRentedPanel.setOpaque(false);
-	    resultsLabel.setOpaque(false);
-	    mostRentedLabel.setOpaque(false);
-	    // Categories
-	    categoriesManager = new JPanel(new GridLayout(0, 1));
-	    categoriesPane = new JScrollPane(categoriesManager, 
-		    ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-	    categoriesPane.setBorder(BorderFactory.createEmptyBorder());
-	    for(Categories c: Categories.values()) {
-		JButton button = new JButton(c.toString());
-		button.setPreferredSize(new Dimension(200, 50));
-		button.setSize(new Dimension(200, 25));
-		categoriesManager.add(button);
-	    }
-	    
-	    this.add(categoriesPane, BorderLayout.WEST);
-	    
-	    /* Center */
-	    JPanel centerPanel = new JPanel(new GridLayout(2,0));
-	    centerPanel.setOpaque(false);
-	    // Results
-	    resultsManager = new FilmManagerPanel(new FlowLayout(FlowLayout.LEFT), 100);	    
-	    resultsPane = new JScrollPane(resultsManager, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, 
-		    ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	    resultsPane.setBorder(BorderFactory.createEmptyBorder());
-	    resultsLabel.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 20));
-	    resultsPanel.add(resultsLabel, BorderLayout.NORTH);
-	    resultsPanel.add(resultsPane, BorderLayout.CENTER);
-	    centerPanel.add(resultsPanel);
-	    
-	    // Most rented films
-	    mostRentedManager = new FilmManagerPanel(new FlowLayout(FlowLayout.LEFT), 100);
-	    mostRentedPane = new JScrollPane(mostRentedManager, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, 
-		    ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	    mostRentedPane.setBorder(BorderFactory.createEmptyBorder());
-	    mostRentedLabel.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 20));
-	    mostRentedPanel.add(mostRentedLabel, BorderLayout.NORTH);
-	    mostRentedPanel.add(mostRentedPane, BorderLayout.CENTER);
-	    centerPanel.add(mostRentedPanel);
+    /* Components */
+    private JPanel mainPanel;
+    /* Results */
+    private JPanel resultsPanel;
+    private JLabel resultsLabel;
+    private JScrollPane resultsPane;
+    private FilmManagerPanel resultsManager;
+    /* Most rented films */
+    private JPanel mostRentedPanel;
+    private JLabel mostRentedLabel;
+    private JScrollPane mostRentedPane;
+    private FilmManagerPanel mostRentedManager;
+    /* Categories */
+    private JScrollPane categoriesPane;
+    private JPanel categoriesManager;
+    /* Film Manager */
+    private FilmManager filmManager;
+    
+    private int countResults;
 
-	    mainPanel.add(centerPanel, BorderLayout.CENTER);
-	    this.add(mainPanel,BorderLayout.CENTER);
+    /**
+     * Default Constructor of {@code SearchPage}
+     * @author MathysC
+     *
+     */
+    public SearchPage() {
+	mainPanel = new JPanel(new BorderLayout());
+	resultsPanel = new JPanel(new BorderLayout());
+	resultsLabel = new JLabel();
+	mostRentedPanel = new JPanel(new BorderLayout());
+	mostRentedLabel = new JLabel();
+	countResults = 0;
+	this.createGUI();
+	
+    }
+	
+    /**
+     * Add a film to the ResultPanel
+     * @author MathysC
+     *
+     * @param film The film to add.
+     */
+    public void addResult(Film film) { 
+	FilmCartPanel panel = this.filmManager.getFilmCartPanel(film);
+	if(panel != null) {
+	    this.resultsManager.addPanel(panel);
+	    countResults++;
+	    this.updateCountResults();
+	}
 	    
-	    this.filmManager = FilmManager.getInstance();
-
-	    GUIManager.getInstance().registerColorTheme(mostRentedManager);
-	    GUIManager.getInstance().registerColorTheme(resultsManager);
-	}
+    }
 	
-	public void addResult(Film film) { 
-	    FilmCartPanel panel = this.filmManager.getFilmCartPanel(film);
-	    if(panel != null) {
-		this.resultsManager.addPanel(panel);
-		countResults++;
-        	this.updateCountResults();
-	    }
+    /**
+     * Update the JLabel of results Found
+     * @author MathysC
+     */
+    private void updateCountResults() {
+	String text = resultsLabel.getText();
+	// Remove the previous count and update it
+	text = text.split(" ")[0] + " (" + countResults + ")";
+	resultsLabel.setText(text);
+    }
+	
+    @Override
+    public void createGUI() {
+	this.setLayout(new BorderLayout());
+	resultsPanel.setOpaque(false);
+	mostRentedPanel.setOpaque(false);
+	resultsLabel.setOpaque(false);
+	mostRentedLabel.setOpaque(false);
+	// Categories
+	categoriesManager = new JPanel(new GridLayout(0, 1));
+	categoriesPane = new JScrollPane(categoriesManager, 
+		ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	categoriesPane.setBorder(BorderFactory.createEmptyBorder());
+	for(Categories c: Categories.values()) {
+	    JButton button = new JButton(c.toString());
+	    button.setPreferredSize(new Dimension(200, 50));
+	    button.setSize(new Dimension(200, 25));
+	    categoriesManager.add(button);
+	}
 	    
-	}
-	
-	/**
-	 * update the JLabel of results Found
-	 * @author MathysC
-	 *
-	 */
-	private void updateCountResults() {
-	    String text = resultsLabel.getText();
-	    // Remove the previous count and update it
-	    text = text.split(" ")[0] + " (" + countResults + ")";
-	    resultsLabel.setText(text);
-	}
-	
-	
-	@Override
-	public void setLanguage(ResourceBundle rb) {
-		resultsLabel.setText(rb.getString("result_title"));
-		this.updateCountResults();
-		mostRentedLabel.setText(rb.getString("result_most_rented"));
-	}
+	this.add(categoriesPane, BorderLayout.WEST);
+	    
+	/* Center */
+	JPanel centerPanel = new JPanel(new GridLayout(2,0));
+	centerPanel.setOpaque(false);
+	// Results
+	resultsManager = new FilmManagerPanel(new FlowLayout(FlowLayout.LEFT), 100);	    
+	resultsPane = new JScrollPane(resultsManager, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, 
+		ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	resultsPane.setBorder(BorderFactory.createEmptyBorder());
+	resultsLabel.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 20));
+	resultsPanel.add(resultsLabel, BorderLayout.NORTH);
+	resultsPanel.add(resultsPane, BorderLayout.CENTER);
+	centerPanel.add(resultsPanel);
+	    
+	// Most rented films
+	mostRentedManager = new FilmManagerPanel(new FlowLayout(FlowLayout.LEFT), 100);
+	mostRentedPane = new JScrollPane(mostRentedManager, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, 
+		ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	mostRentedPane.setBorder(BorderFactory.createEmptyBorder());
+	mostRentedLabel.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 20));
+	mostRentedPanel.add(mostRentedLabel, BorderLayout.NORTH);
+	mostRentedPanel.add(mostRentedPane, BorderLayout.CENTER);
+	centerPanel.add(mostRentedPanel);
 
-	@Override
-	public void setColorTheme(ColorThemes colorTheme) {
-	    switch(colorTheme) {
+	mainPanel.add(centerPanel, BorderLayout.CENTER);
+	this.add(mainPanel,BorderLayout.CENTER);
+	    
+	this.filmManager = FilmManager.getInstance();
+
+	GUIManager.getInstance().registerColorTheme(mostRentedManager);
+	GUIManager.getInstance().registerColorTheme(resultsManager);
+	    
+    }
+	
+    @Override
+    public void setLanguage(ResourceBundle rb) {
+	resultsLabel.setText(rb.getString("result_title"));
+	this.updateCountResults();
+	mostRentedLabel.setText(rb.getString("result_most_rented"));
+    }
+
+    @Override
+    public void setColorTheme(ColorThemes colorTheme) {
+	switch(colorTheme) {
 	    case LIGHTTHEME:
 		mainPanel.setBackground(Light.BG.getColor());
-		    resultsLabel.setForeground(Light.REVERSE_FG.getColor());
-		    mostRentedLabel.setForeground(Light.REVERSE_FG.getColor());
+		resultsLabel.setForeground(Light.REVERSE_FG.getColor());
+		mostRentedLabel.setForeground(Light.REVERSE_FG.getColor());
 
-		    for(Component c: categoriesManager.getComponents()) {
-			JButton button = (JButton)c;
-			button.setBackground(Light.BLUE.getColor());
-			button.setForeground(Light.WHITE.getColor());
-			button.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 12));
-		    }
+		for(Component c: categoriesManager.getComponents()) {
+		    JButton button = (JButton)c;
+		    button.setBackground(Light.BLUE.getColor());
+		    button.setForeground(Light.WHITE.getColor());
+		    button.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 12));
+		}
 		break;
 	    case DARKTHEME:
 		mainPanel.setBackground(Dark.BG.getColor());
-		    resultsLabel.setForeground(Dark.FOREGROUND.getColor());
-		    mostRentedLabel.setForeground(Dark.FOREGROUND.getColor());	
+		resultsLabel.setForeground(Dark.FOREGROUND.getColor());
+		mostRentedLabel.setForeground(Dark.FOREGROUND.getColor());	
 
-		    for(Component c: categoriesManager.getComponents()) {
-			JButton button = (JButton)c;
-			button.setBackground(Dark.BLUE.getColor());
-			button.setForeground(Dark.FOREGROUND.getColor());
-			button.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 12));
-		    }
+		for(Component c: categoriesManager.getComponents()) {
+		    JButton button = (JButton)c;
+		    button.setBackground(Dark.BLUE.getColor());
+		    button.setForeground(Dark.FOREGROUND.getColor());
+		    button.setFont(Decorations.FONT_BASIC.getFont(Font.BOLD, 12));
+		}
 		break;
 	    default:
 		break;
-	    }	    
 	}
+	    
+    }
+
 }
