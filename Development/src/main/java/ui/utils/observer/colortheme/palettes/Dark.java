008a12fa2e2d2f8deed7f75280d9c22442ba2713
Clerget Mathys <mathysclerget+github@gmail.com>
2022-12-10 17:16:57 +0100

    fix: Colors #42

--- Development/src/main/java/ui/utils/observer/colortheme/palettes/Dark.java
+++ Development/src/main/java/ui/utils/observer/colortheme/palettes/Dark.java
@@ -10,58 +10,24 @@ import java.awt.Color;
  * @see https://draculatheme.com/contribute
  */
 public enum Dark {
-
-	BG(40,42,44),
-	FOREGROUND(248, 248, 242),
-	PURPLE(86, 71, 90),
-	BLUE(98, 114, 164),
-	PINK(189, 147, 249);
+    BG(new Color(40,42,44)),
+    FOREGROUND(new Color(248, 248, 242)),
+    PURPLE(new Color(86, 71, 90)),
+    BLUE(new Color(98, 114, 164)),
+    PINK(new Color(189, 147, 249));
 	
 	
-	private int[] rgb;
+    private Color color;
+    
+    Dark(Color c) {
+	this.color = c;
+    }
 	
-	Dark(int r, int g, int b) {
-		this.rgb = new int[]{r, g, b};
-	}
-	
-	/**
-	 * Get the color.
-	 * @author MathysC
-	 *
-	 * @return Color from rgb.
-	 */
-	public Color getColor() {
-		return new Color(this.r(), this.g(), this.b());
-		
-	}
-	
-	/**
-	 * Get red value
-	 * @author MathysC
-	 *
-	 * @return int red
-	 */
-	private int r() {
-		return this.rgb[0];
-	}
-	
-	/**
-	 * Get green value
-	 * @author MathysC
-	 *
-	 * @return int green
-	 */
-	private int g() {
-		return this.rgb[1];
-	}
-	
-	/**
-	 * Get blue value
-	 * @author MathysC
-	 *
-	 * @return int blue
-	 */
-	private int b() {
-		return this.rgb[2];
-	}
+    /**
+     * @author MathysC
+     * @return Color the color to get.
+     */
+    public Color getColor() {
+	return this.color;	
+    }
 }
--- Development/src/main/java/ui/utils/observer/colortheme/palettes/Light.java
+++ Development/src/main/java/ui/utils/observer/colortheme/palettes/Light.java
@@ -9,58 +9,24 @@ import java.awt.Color;
  *
  */
 public enum Light {
-	BG(238, 239, 240),
-	REVERSE_FG(47, 83, 170),	// Foreground Blue used on white buttons.
-	REVERSE_BG(255, 255, 255),	// Background White used on white buttons.
-	BLACK(0, 0 ,0),
-	BLUE(47, 63, 170),
-	WHITE(245, 246, 243);
+    BG(new Color(238, 239, 240)),
+    REVERSE_FG(new Color(47, 83, 170)), // Foreground Blue used on white buttons.
+    REVERSE_BG(new Color(255, 255, 255)), // Background White used on white buttons.
+    BLACK(Color.BLACK),
+    BLUE(new Color(47, 63, 170)),
+    WHITE(new Color(245, 246, 243));
+    
+    private Color color;
+    
+    Light(Color c){ 
+	this.color = c;
+    }
 	
-	
-	
-	private int[] rgb;
-	
-	Light(int r, int g, int b) {
-		this.rgb = new int[]{r, g, b};
-	}
-	
-	/**
-	 * Get the color.
-	 * @author MathysC
-	 *
-	 * @return Color from rgb.
-	 */
-	public Color getColor() {
-		return new Color(this.r(), this.g(), this.b());
-	}
-	
-	/**
-	 * Get red value
-	 * @author MathysC
-	 *
-	 * @return int red
-	 */
-	private int r() {
-		return this.rgb[0];
-	}
-	
-	/**
-	 * Get green value
-	 * @author MathysC
-	 *
-	 * @return int green
-	 */
-	private int g() {
-		return this.rgb[1];
-	}
-	
-	/**
-	 * Get blue value
-	 * @author MathysC
-	 *
-	 * @return int blue
-	 */
-	private int b() {
-		return this.rgb[2];
-	}
+    /**
+     * @author MathysC
+     * @return Color the color to get.
+     */
+    public Color getColor() {
+	return this.color;
+    }
 }
