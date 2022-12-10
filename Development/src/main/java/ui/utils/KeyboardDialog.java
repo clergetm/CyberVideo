c7227fd5fe7b39006a4a1acf7c116fa0dbf157bd
Clerget Mathys <mathysclerget+github@gmail.com>
2022-12-10 16:15:41 +0100

    fix: KeyboardDialog #42

--- Development/src/main/java/ui/utils/KeyboardDialog.java
+++ Development/src/main/java/ui/utils/KeyboardDialog.java
@@ -16,6 +16,7 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import ui.GUIComponent;
 import ui.utils.observer.colortheme.ColorThemes;
 import ui.utils.observer.colortheme.IColorThemeObserver;
 import ui.utils.observer.colortheme.palettes.Dark;
@@ -27,17 +28,21 @@ import ui.utils.observer.multilingual.IMultilingualObserver;
  * @author MathysC
  */
 @SuppressWarnings("serial")
-public class KeyboardDialog extends JDialog implements ActionListener, IMultilingualObserver, IColorThemeObserver {
+public class KeyboardDialog extends JDialog implements GUIComponent, ActionListener, IMultilingualObserver, IColorThemeObserver {
 
-    private String entry = ""; // text of the TextField.
-
+    /* Actions */
+    public static final String VAL_ACT = "Validate";
+    public static final String BSPACE_ACT = "Back_space";
+    public static final String SPACE_ACT = "Space";
+    /* Icons */
+    public static final String ICO_KEYBOARD = "Keyboard";
     /* Characters */
     // Alphabet in lower and upper cases.
     private static final int UPPERCASE_A = 65;
     private static final int UPPERCASE_Z = 91;
     private static final int LOWERCASE_A = 97;
     private static final int LOWERCASE_Z = 123;
-    private final char[] specialChars = {
+    private static final char[] SPECIAL_CHARS = {
         '!', '@', '#', '$', '%',
         '&', '*', '(', ')', '-',
         '_', '=', '+', '[', ']',
@@ -46,30 +51,19 @@ public class KeyboardDialog extends JDialog implements ActionListener, IMultilin
         ';', ':' };
     
     /* Components */
-    private JTextField textField = new JTextField(100);
-    private JPanel keysPanel = new JPanel();
-    private JPanel specialPanel = new JPanel();
-    private JPanel actionsPanel = new JPanel();
-    private JPanel lowerCasePanel = new JPanel();
-    private JPanel upperCasePanel = new JPanel();
-    private JButton valButton = new JButton();
-    private JButton bspaceButton = new JButton();
-    private JButton spaceButton = new JButton();
-    private JButton[] actionButtons = {
-        valButton,
-        bspaceButton,
-        spaceButton
-    };
-    
-    /* Actions */
-    public static final String VAL_ACT = "Validate";
-    public static final String BSPACE_ACT = "Back_space";
-    public static final String SPACE_ACT = "Space";
-    
-    /* Icons */
-    public static final String ICO_KEYBOARD = "Keyboard";
-	
-    private static KeyboardDialog instance;
+    private JTextField textField;
+    private JPanel keysPanel;
+    private JPanel specialPanel;
+    private JPanel actionsPanel;
+    private JPanel lowerCasePanel;
+    private JPanel upperCasePanel;
+    private JButton valButton;
+    private JButton bspaceButton;
+    private JButton spaceButton;
+    private JButton[] actionButtons;
+       
+    private String entry = ""; // text of the TextField.
+    private static KeyboardDialog instance = null;
     
     /**
      * Constructor of {@code VirtualKeyboard} 
@@ -80,111 +74,39 @@ public class KeyboardDialog extends JDialog implements ActionListener, IMultilin
     private KeyboardDialog() {
         // Set Options.
         super(null, Dialog.ModalityType.APPLICATION_MODAL);
-        this.createGUI();
-    }
-    private void createGUI() {
-	this.setIconImage(Resources.getIco(ICO_KEYBOARD));
+        this.setIconImage(Resources.getIco(ICO_KEYBOARD));
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setLocation(10, 400);
-
-        // Add Components.
-        keysPanel.setLayout(new BoxLayout(keysPanel, BoxLayout.Y_AXIS));
         
-        lowerCasePanel = getLowerCaseKeys();
-        upperCasePanel = getUpperCaseKeys();
-        specialPanel = getSpecialKeys();
-        keysPanel.add(lowerCasePanel);
-        keysPanel.add(upperCasePanel);
-        keysPanel.add(specialPanel);
-        this.add(keysPanel, BorderLayout.CENTER);
-
-        actionsPanel = getActionKeys();
-        this.add(actionsPanel, BorderLayout.EAST);
+        textField = new JTextField(100);
+       	keysPanel = new JPanel();
+       	
+       	lowerCasePanel = new JPanel();
+       	upperCasePanel = new JPanel();
+       	specialPanel = new JPanel();
+       	actionsPanel = new JPanel();
+       	
+       	valButton = new JButton();
+       	bspaceButton = new JButton();
+       	spaceButton = new JButton();
+       	actionButtons =  new JButton []{ valButton, bspaceButton, spaceButton };
        
-        this.pack();
+       	this.createGUI();
     }
-
+        
+    /**
+     * Lazy Singleton Pattern implementation.
+     * @author MathysC
+     *
+     * @return the one and only instance of the {@code KeyBoardDialog}
+     */
     public static KeyboardDialog getInstance() {
 	if(instance == null) {
 	    instance = new KeyboardDialog();
 	}
 	
 	return instance;
-    }
-    /**
-     * Implements the lowercase alphabet.
-     * Method implemented for better readability.
-     * @author MathysC
-     * 
-     * @return A JPanel of lowercased letters
-     */
-    private JPanel getLowerCaseKeys() {
-        JPanel lowerKeys = new JPanel();
-        for (int letter = LOWERCASE_A; letter < LOWERCASE_Z; letter++) {
-            JButton button = new JButton("" + ((char) letter));
-            button.addActionListener(this);
-            lowerKeys.add(button);
-        }
-        return lowerKeys;
-    }
-
-    /**
-     * Implements the uppercase alphabet.
-     * Method implemented for better readability.
-     * @author MathysC
-     * 
-     * @return A JPanel of uppercased letters
-     */
-    private JPanel getUpperCaseKeys() {
-        JPanel upperKeys = new JPanel();
-        for (int letter = UPPERCASE_A; letter < UPPERCASE_Z; letter++) {
-            JButton button = new JButton("" + ((char) letter));
-            button.addActionListener(this);
-            upperKeys.add(button);
-        }
-        return upperKeys;
-    }
-
-    /**
-     * Implements the special characters.
-     * Method implemented for better readability
-     * @author MathysC
-     *
-     * @return A JPanel of special characters.
-     */
-    private JPanel getSpecialKeys() {
-        JPanel specials = new JPanel();
-        for (char c: specialChars) {
-            JButton button = new JButton("" + c);
-            button.addActionListener(this);
-            specials.add(button);
-        }
-        return specials;
-    }
-
-    /**
-     * Implements the actions buttons.
-     * Method implemented for better readability
-     * @author MathysC
-     *
-     * @return A JPanel of action buttons.
-     */
-    private JPanel getActionKeys() {
-        final String[] actionCommands = {
-            VAL_ACT,
-            BSPACE_ACT,
-            SPACE_ACT,
-        };
-
-        JPanel actions = new JPanel(new GridLayout(3, 0));
-        actions.setBorder(Decorations.getDefaultBorder());
-        for (int index = 0; index < actionCommands.length; index++) {
-            this.actionButtons[index].setActionCommand(actionCommands[index]);
-            this.actionButtons[index].addActionListener(this);
-            actions.add(this.actionButtons[index]);
-        }
-        return actions;
     }
 
     /**
@@ -193,7 +115,7 @@ public class KeyboardDialog extends JDialog implements ActionListener, IMultilin
      *
      * @return The entry String.
      */
-    public String getEntry() {
+    private String getEntry() {
         return this.entry;
     }
 
@@ -238,7 +160,53 @@ public class KeyboardDialog extends JDialog implements ActionListener, IMultilin
             break;
         }
     }
+    
+    @Override
+    public void createGUI() {
+        // Add Components.
+        keysPanel.setLayout(new BoxLayout(keysPanel, BoxLayout.Y_AXIS));
+        
+        /* Implements the lowercase alphabet. */
+        for (int letter = LOWERCASE_A; letter < LOWERCASE_Z; letter++) {
+            JButton button = new JButton("" + ((char) letter));
+            button.addActionListener(this);
+            lowerCasePanel.add(button);
+        }
+        
+        /* Implements the uppercase alphabet. */
+        for (int letter = UPPERCASE_A; letter < UPPERCASE_Z; letter++) {
+            JButton button = new JButton("" + ((char) letter));
+            button.addActionListener(this);
+            upperCasePanel.add(button);
+        }
+        
+        /* Implements the special characters. */
+        for (char c: SPECIAL_CHARS) {
+            JButton button = new JButton("" + c);
+            button.addActionListener(this);
+            specialPanel.add(button);
+        }
+        
+        keysPanel.add(lowerCasePanel);
+        keysPanel.add(upperCasePanel);
+        keysPanel.add(specialPanel);
+        this.add(keysPanel, BorderLayout.CENTER);
+       
+        /* Implements the actions buttons. */
+        final String[] actionCommands = { VAL_ACT, BSPACE_ACT, SPACE_ACT };
 
+        actionsPanel = new JPanel(new GridLayout(3, 0));
+        actionsPanel.setBorder(Decorations.getDefaultBorder());
+        for (int index = 0; index < actionCommands.length; index++) {
+            this.actionButtons[index].setActionCommand(actionCommands[index]);
+            this.actionButtons[index].addActionListener(this);
+            actionsPanel.add(this.actionButtons[index]);
+        }
+        this.add(actionsPanel, BorderLayout.EAST);
+       
+        this.pack();
+    }
+    
     /**
      * Set the action button texts in chosen Language.
      * @author MathysC
@@ -252,70 +220,70 @@ public class KeyboardDialog extends JDialog implements ActionListener, IMultilin
         }
     }
 
-	@Override
-	public void setColorTheme(ColorThemes colorTheme) {
-	    switch(colorTheme) {
-	    case LIGHTTHEME:
-		// This Dialog.
-		this.setBackground(Light.BG.getColor());
+    @Override
+    public void setColorTheme(ColorThemes colorTheme) {
+	switch(colorTheme) {
+	case LIGHTTHEME:
+	    // This Dialog.
+	    this.setBackground(Light.BG.getColor());
 
-		// Characters.
-		for(Component panel :this.keysPanel.getComponents()) {
-			panel.setBackground(Light.BG.getColor());
-			for(Component key : ((JPanel) panel).getComponents()) {
-				key.setBackground(Light.BLUE.getColor());
-				key.setForeground(Light.WHITE.getColor());
-			}
+	    // Characters.
+	    for(Component panel :this.keysPanel.getComponents()) {
+		panel.setBackground(Light.BG.getColor());
+		for(Component key : ((JPanel) panel).getComponents()) {
+		    key.setBackground(Light.BLUE.getColor());
+		    key.setForeground(Light.WHITE.getColor());
 		}
-		
-		// Actions Panel.
-		this.actionsPanel.setBackground(Light.BG.getColor());
-		
-		// Validate Button.
-		this.valButton.setBackground(new Color(217, 221, 146));
-		this.valButton.setForeground(Light.BLACK.getColor());
-
-		// Back Space Button.
-		this.bspaceButton.setBackground(new Color(238, 99, 82));
-		this.bspaceButton.setForeground(Light.WHITE.getColor());
-		
-		// Space Button.
-		this.spaceButton.setBackground(Light.WHITE.getColor());
-		this.spaceButton.setForeground(Light.REVERSE_FG.getColor());
-		break;
-	    case DARKTHEME:
-		// This Dialog.
-		this.setBackground(Dark.BG.getColor());
-
-		// Characters.
-		for(Component panel :this.keysPanel.getComponents()) {
-			panel.setBackground(Dark.BG.getColor());
-			for(Component key : ((JPanel) panel).getComponents()) {
-				key.setBackground(Dark.BLUE.getColor());
-				key.setForeground(Dark.FOREGROUND.getColor());
-			}
-		}
-		
-		// Actions Panel.
-		this.actionsPanel.setBackground(Dark.BG.getColor());
-		
-		// Validate Button.
-		this.valButton.setBackground(new Color(127, 176, 105));
-		this.valButton.setForeground(Dark.FOREGROUND.getColor());
-
-		// Back Space Button.
-		this.bspaceButton.setBackground(new Color(211, 97, 53));
-		this.bspaceButton.setForeground(Dark.FOREGROUND.getColor());
-		
-		// Space Button.
-		this.spaceButton.setBackground(Dark.PURPLE.getColor());
-		this.spaceButton.setForeground(Dark.FOREGROUND.getColor());
-		break;
-	    default:
-		break;
 	    }
+		
+	    // Actions Panel.
+	    this.actionsPanel.setBackground(Light.BG.getColor());
+		
+	    // Validate Button.
+	    this.valButton.setBackground(new Color(217, 221, 146));
+	    this.valButton.setForeground(Light.BLACK.getColor());
+
+	    // Back Space Button.
+	    this.bspaceButton.setBackground(new Color(238, 99, 82));
+	    this.bspaceButton.setForeground(Light.WHITE.getColor());
+		
+	    // Space Button.
+	    this.spaceButton.setBackground(Light.WHITE.getColor());
+	    this.spaceButton.setForeground(Light.REVERSE_FG.getColor());
+	    break;
+	case DARKTHEME:
+	    // This Dialog.
+	    this.setBackground(Dark.BG.getColor());
+	    // Characters.
+	    for(Component panel :this.keysPanel.getComponents()) {
+		panel.setBackground(Dark.BG.getColor());
+		for(Component key : ((JPanel) panel).getComponents()) {
+		    key.setBackground(Dark.BLUE.getColor());
+		    key.setForeground(Dark.FOREGROUND.getColor());
+		}
+	    }
+		
+	    // Actions Panel.
+	    this.actionsPanel.setBackground(Dark.BG.getColor());
 	    
+	    // Validate Button.
+	    this.valButton.setBackground(new Color(127, 176, 105));
+	    this.valButton.setForeground(Dark.FOREGROUND.getColor());
+
+	    // Back Space Button.
+	    this.bspaceButton.setBackground(new Color(211, 97, 53));
+	    this.bspaceButton.setForeground(Dark.FOREGROUND.getColor());
+		
+	    // Space Button.
+	    this.spaceButton.setBackground(Dark.PURPLE.getColor());
+	    this.spaceButton.setForeground(Dark.FOREGROUND.getColor());
+		
+	    break;
 	    
+	default:
+	    break;
 	}
+
+    }
 
 }
