a657d68b235b28e906951a33416f7bfe2fa32bd2
Clarisse Deschamps <cdeschamps2811@gmail.com>
2022-12-11 20:47:35 +0100

    feat: AL2000 Add all necessary comments #63

--- Development/src/main/java/fc/al2000/AL2000.java
+++ Development/src/main/java/fc/al2000/AL2000.java
@@ -4,9 +4,8 @@ import fc.clients.Client;
 import fc.clients.cards.Card;
 
 /**
- * 
  * @author Clarisse
- *
+ * Class that allows to represent the AL2000 machine
  */
 
 public class AL2000 {
@@ -25,30 +24,52 @@ public class AL2000 {
 	public AL2000() {
 	}
 	
+	/**
+	 * @return the card of the client who is connected
+	 */
 	public Card getCard() {
 		return connectedClient.getCreditCard();
 	}
 	
+	/**
+	 * @return the printer of AL2000
+	 */
 	public Printer getPrinter() {
 		return printer;
 	}
 	
+	/**
+	 * @return the bank
+	 */
 	public Bank getBank() {
 		return bank;
 	}
 	
+	/**
+	 * @return the electronic payment terminal
+	 */
 	public EPT getEPT() {
 		return ept;
 	}
 	
+	/**
+	 * @return the distributor
+	 */
 	public Distributor getDistributor() {
 		return ditributor;
 	}
 	
+	/**
+	 * @return current client who is connected
+	 */
 	public Client getConnectedClient() {
 		return connectedClient;
 	}
 
+	/**
+	 * change the current client
+	 * @param connectedClient the client who is conecting
+	 */
 	public void setConnectedClient(Client connectedClient) {
 		this.connectedClient = connectedClient;
 	}
--- Development/src/main/java/fc/al2000/Distributor.java
+++ Development/src/main/java/fc/al2000/Distributor.java
@@ -5,12 +5,10 @@ import fc.films.BluRay;
 /**
  * Class that represents the distributor of the AL2000
  * @author Clarisse
- *
  */
 public class Distributor {
 	
 	/**
-	 * 
 	 * @param br the BluRay to distribute at the client
 	 * @return true if the bluray was distributed
 	 */
@@ -19,12 +17,10 @@ public class Distributor {
 	}
 	
 	/**
-	 * 
 	 * @param br the BluRay to receive from the client
 	 * @return true if the bluray was receive
 	 */
 	public boolean receive(BluRay br) {
 		return true;
 	}
-	
 }
