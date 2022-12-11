f4bdab6559755c525499e99c96309a2f30439f13
Clerget Mathys <mathysclerget+github@gmail.com>
2022-12-11 22:29:02 +0100

    fix: Tests

--- Development/src/test/java/fc/Films/BluRayTest.java
+++ Development/src/test/java/fc/Films/BluRayTest.java
@@ -7,53 +7,51 @@ import fc.films.BluRay;
 import fc.films.StatesBluRay;
 
 class BluRayTest {
-
-	/* Create Film attributes */
-	private final double price = 20;
-	private final StatesBluRay state = StatesBluRay.AVAILABLE;
+    /* Create Film attributes */
+    private final double price = 20;
+    private final StatesBluRay state = StatesBluRay.AVAILABLE;
 	
-	/* Create an instance of a BluRay for test BluRay class */
-	protected BluRay bluRay = new BluRay(price, state);
-	
-	
-	@Test
-	void testGetType() {
-		Assertions.assertEquals("BluRay", bluRay.getType());
-	    Assertions.assertNotEquals("QRCode", bluRay.getType());
-	}
+    /* Create an instance of a BluRay for test BluRay class */
+    protected BluRay bluRay = new BluRay(price, state);
 
-	@Test
-	void testGetTariff() {
-	    Assertions.assertEquals(5, bluRay.getTariff());
-	}
+    @Test
+    void testGetType() {
+	Assertions.assertEquals("BluRay", bluRay.getType());
+	Assertions.assertNotEquals("QRCode", bluRay.getType());
+    }
 
-	@Test
-	void testIsAvailable() {
-		Assertions.assertTrue(bluRay.isAvailable());
-	}
+    @Test
+    void testGetTariff() {
+	Assertions.assertEquals(5, bluRay.getTariff());
+    }
 
-	@Test
-	void testBluRay() {
-		Assertions.assertEquals(price, bluRay.getPurchasePrice());
-		Assertions.assertEquals(state, StatesBluRay.AVAILABLE);	
-	}
+    @Test
+    void testIsAvailable() {
+	Assertions.assertTrue(bluRay.isAvailable());
+    }
 
-	@Test
-	void testGetPurchasePrice() {
-		Assertions.assertEquals(price, bluRay.getPurchasePrice());
-	}
+    @Test
+    void testBluRay() {
+	Assertions.assertEquals(price, bluRay.getPurchasePrice());
+	Assertions.assertEquals(state, StatesBluRay.AVAILABLE);	
+    }
 
-	@Test
-	void testGetState() {
-		Assertions.assertEquals(state, bluRay.getState());
-	}
+    @Test
+    void testGetPurchasePrice() {
+	Assertions.assertEquals(price, bluRay.getPurchasePrice());
+    }
 
-	@Test
-	void testSetState() {
-		bluRay.setState(StatesBluRay.RENTED);
-		Assertions.assertEquals(StatesBluRay.RENTED, bluRay.getState());
-		Assertions.assertNotEquals(StatesBluRay.AVAILABLE, bluRay.getState());
-		bluRay.setState(StatesBluRay.AVAILABLE);
+    @Test
+    void testGetState() {
+	Assertions.assertEquals(state, bluRay.getState());
+    }
 
-	}
+    @Test
+    void testSetState() {
+	bluRay.setState(StatesBluRay.RENTED);
+	Assertions.assertEquals(StatesBluRay.RENTED, bluRay.getState());
+	Assertions.assertNotEquals(StatesBluRay.AVAILABLE, bluRay.getState());
+	bluRay.setState(StatesBluRay.AVAILABLE);
+    }
+    
 }
--- Development/src/test/java/fc/Films/FilmTest.java
+++ Development/src/test/java/fc/Films/FilmTest.java
@@ -1,6 +1,8 @@
 package fc.Films;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,77 +17,75 @@ import fc.films.StatesBluRay;
 import fc.films.Support;
 
 class FilmTest implements LifecycleLoggerTest{
+    /* Create Film attributes */
+    protected final String title = "The Matrix";
+    protected final String synopsis = "Thomas A. Anderson is a man living two lives. "
+	    + "By day he is an average computer programmer and by night "
+	    + "a hacker known as Neo. Neo has always questioned his reality,"
+	    + " but the truth is far beyond his imagination...";
+    protected final String director_lname = "Wachowski";
+    protected final String director_fname = "Lana";
+    protected final ArrayList<String> actors = new ArrayList<>(Arrays.asList("Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving"));
+    protected final AgeRestriction restriction = AgeRestriction.EVERYONE;
+    protected final ArrayList<Categories> categories = new ArrayList<>(Arrays.asList(Categories.ACTION, Categories.DRAMAS));
+    protected final Year year = Year.of(1999);
+    protected final ArrayList<Support> supports = new ArrayList<>(Arrays.asList(new BluRay(22.5, StatesBluRay.RENTED), new QRCode(""), new BluRay(22.50, StatesBluRay.AVAILABLE)));
 
+    /* Create an instance of a Film for test Film class */
+    protected final Film film = new Film(title, synopsis, actors, director_fname, director_lname, year, categories, restriction, supports);
+		
 	
-	/* Create Film attributes */
-	private final String title = "The Matrix",
-			synopsis = "Thomas A. Anderson is a man living two lives. "
-					+ "By day he is an average computer programmer and by night "
-					+ "a hacker known as Neo. Neo has always questioned his reality,"
-					+ " but the truth is far beyond his imagination...",
-			director_lname = "Wachowski",
-			director_fname = "Lana";
-	private final String[] actors = {"Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving"};
-	private final AgeRestriction restriction = AgeRestriction.EVERYONE;
-	private final Categories[] categories = {Categories.ACTION, Categories.DRAMAS};
-	private final Year year = Year.of(1999);
-	private final Support[] supports = {new BluRay(22.5, StatesBluRay.RENTED), new QRCode(), new BluRay(22.50, StatesBluRay.AVAILABLE)};
-	
-	/* Create an instance of a Film for test Film class */
-	protected Film film = new Film(title, synopsis, actors, director_fname, director_lname, year, categories, restriction, supports);
-	
-	
-	@Test
-	void testFilm() {
-		Assertions.assertEquals(title, film.getTitle());
-		Assertions.assertEquals(synopsis, film.getSynopsis());
-		Assertions.assertEquals(actors, film.getActors());
-		Assertions.assertEquals(director_fname, film.getNamesDirector().substring(0, film.getNamesDirector().indexOf(" ")));
-		Assertions.assertEquals(director_lname, film.getNamesDirector().substring(film.getNamesDirector().indexOf(" ")+1));
-		Assertions.assertEquals(year, film.getYear());
-		Assertions.assertEquals(categories, film.getCategories());
-		Assertions.assertEquals(restriction, film.getRestriction());
-		Assertions.assertEquals(supports, film.getSupports());
-	}
+    @Test
+    void testFilm() {
+	Assertions.assertEquals(title, film.getTitle());
+	Assertions.assertEquals(synopsis, film.getSynopsis());
+	Assertions.assertEquals(actors, film.getActors());
+	Assertions.assertEquals(director_fname, film.getNamesDirector().substring(0, film.getNamesDirector().indexOf(" ")));
+	Assertions.assertEquals(director_lname, film.getNamesDirector().substring(film.getNamesDirector().indexOf(" ")+1));
+	Assertions.assertEquals(year, film.getYear());
+	Assertions.assertEquals(categories, film.getCategories());
+	Assertions.assertEquals(restriction, film.getRestriction());
+	Assertions.assertEquals(supports, film.getSupports());
+    }
 
-	@Test
-	void testGetTitle() {
-		Assertions.assertEquals(title, film.getTitle());
-	}
+    @Test
+    void testGetTitle() {
+	Assertions.assertEquals(title, film.getTitle());
+    }
 
-	@Test
-	void testGetSynopsis() {
-		Assertions.assertEquals(synopsis, film.getSynopsis());
-	}
+    @Test
+    void testGetSynopsis() {
+	Assertions.assertEquals(synopsis, film.getSynopsis());
+    }
 
-	@Test
-	void testGetActors() {
-		Assertions.assertEquals(actors, film.getActors());
-	}
+    @Test
+    void testGetActors() {
+	Assertions.assertEquals(actors, film.getActors());
+    }
+    
+    @Test
+    void testGetNamesDirector() {
+	Assertions.assertEquals(director_fname+" "+director_lname, film.getNamesDirector());
+    }
 
-	@Test
-	void testGetNamesDirector() {
-		Assertions.assertEquals(director_fname+" "+director_lname, film.getNamesDirector());
-	}
+    @Test
+    void testGetYear() {
+	Assertions.assertEquals(year, film.getYear());
+    }
 
-	@Test
-	void testGetYear() {
-		Assertions.assertEquals(year, film.getYear());
-	}
+    @Test
+    void testGetCategories() {
+	Assertions.assertEquals(categories, film.getCategories());
+    }
 
-	@Test
-	void testGetCategories() {
-		Assertions.assertEquals(categories, film.getCategories());
-	}
+    @Test
+    final void testGetRestriction() {
+	Assertions.assertEquals(restriction, film.getRestriction());
+    }
 
-	@Test
-	final void testGetRestriction() {
-		Assertions.assertEquals(restriction, film.getRestriction());
-	}
-
-	@Test
-	void testGetSupportsType() {
-		Assertions.assertEquals(supports, film.getSupports());
-	}
+    @Test
+    void testGetSupportsType() {
+	Assertions.assertEquals(supports, film.getSupports());
+    }
 
 }
