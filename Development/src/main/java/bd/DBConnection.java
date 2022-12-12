955f6a408673c76b31051c959ce31a8d2183de1a
Clerget Mathys <mathysclerget+github@gmail.com>
2022-12-12 10:01:39 +0100

    feat: Singleton of connection

--- Development/src/main/java/bd/DBConnection.java
+++ Development/src/main/java/bd/DBConnection.java
@@ -1,12 +1,19 @@
 package bd;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * Singleton of connection to the Database.
  * @author MathysC
  */
 public class DBConnection {
 
-    private static DBConnection instance = null;
+    private static Connection instance = null;
     
     /**
      * Private default constructor of {@code DBConnection}
@@ -14,18 +21,40 @@ public class DBConnection {
      *
      */
     private DBConnection() {
-	
+	try {
+	    Properties dbProps = new Properties();
+	    FileInputStream fis = new FileInputStream(Path.CREDENTIALS.getPath("oracleLogs.properties")); 
+	    dbProps.load(fis);
+	    connect(dbProps.getProperty("URL"),
+		    dbProps.getProperty("User"),
+		    dbProps.getProperty("Password")); 
+
+	} catch (IOException | SQLException e) {
+	    e.printStackTrace();
+	}
     }
     
+    /**
+     * Connect the instance.
+     * @author MathysC
+     *
+     * @param URL The URL of the connection
+     * @param user The user.
+     * @param pwd The password of the user.
+     * @throws SQLException
+     */
+    private static void connect(String URL, String user, String pwd) throws SQLException {
+	instance = DriverManager.getConnection(URL, user, pwd);
+    }
     /**
      * Getter of the one and only instance of {@code DBConnection}.
      * @author MathysC
      *
      * @return The Database Connection.
      */
-    public static DBConnection getInstance() {
+    public static Connection getInstance() {
 	if(instance == null) {
-	    instance = new DBConnection();
+	    new DBConnection();
 	}
 	return instance;
     }
