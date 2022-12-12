42c1d54600932a753ccb98169366c600faf63c5a
Evan1700 <evan17.pereira@gmail.com>
2022-12-12 01:56:20 +0100

    Fix: changes for RentalDAO class with the new Rental class

--- Development/src/main/java/bd/DAO_Classes/RentalDAO.java
+++ Development/src/main/java/bd/DAO_Classes/RentalDAO.java
@@ -3,10 +3,14 @@ package bd.DAO_Classes;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import fc.Rental;
 import fc.clients.cards.Card;
+import fc.films.BluRay;
 import fc.films.Film;
+import fc.films.QRCode;
+import fc.films.StatesBluRay;
 import fc.films.Support;
 
 public class RentalDAO extends DAO<Rental> {
@@ -28,8 +32,15 @@ public class RentalDAO extends DAO<Rental> {
         	//FIXME
                 FilmsDAO filmDAO = new FilmsDAO(connect);
                 Film film = filmDAO.read(result.getInt("filmID"));
-                Card card = null;
+                Card card = Card.valueOf(result.getInt("supportCardID"));
+
                 Support support = null;
+                ResultSet resultSuppID = this.connect.createStatement().executeQuery("SELECT price, state FROM BluRays NATURAL JOIN WHERE supportFilmID="+result.getString("supportFilmID"));
+                ResultSet resultSuppIDbis = this.connect.createStatement().executeQuery("SELECT link FROM QRCodes NATURAL JOIN WHERE supportFilmID="+result.getString("supportFilmID"));
+                if(resultSuppID.first())
+                    support = new BluRay(resultSuppID.getInt("price"),StatesBluRay.valueOf(resultSuppID.getString("state")));
+                else if(resultSuppIDbis.first())
+                    support = new QRCode(resultSuppIDbis.getString("link"));
                 rental = new Rental(film, card, support);
             }
 
