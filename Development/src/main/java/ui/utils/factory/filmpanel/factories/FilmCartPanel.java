23647f17437a06749e01859b789d26eee057e0fa
Clerget Mathys <mathysclerget+github@gmail.com>
2022-12-12 16:59:29 +0100

    fix: documentation

--- Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmCartPanel.java
+++ Development/src/main/java/ui/utils/factory/filmpanel/factories/FilmCartPanel.java
@@ -4,22 +4,26 @@ import fc.films.Film;
 import ui.utils.factory.filmpanel.products.CartButton;
 
 /**
- * Factory to create FilmCartPanel.
+ * Factory to create {@code FilmCartPanel}.
  * @author MathysC
  *
  */
 @SuppressWarnings("serial")
 public class FilmCartPanel extends FilmPanel {
 
+    /**
+     * Default constructor of {@code FilmCartPanel}
+     * @author MathysC
+     *
+     * @param film The Film.
+     */
     public FilmCartPanel(Film film) {
 	super(film);
     }
 
     @Override
     protected CartButton createButton(String supportType) {
-	CartButton button = new CartButton(this.getFilm(), supportType);
-
-	
+	CartButton button = new CartButton(this.getFilm(), supportType);	
 	this.buttonPanel.add(button); // Add to the panel 
 	return button;
     }
